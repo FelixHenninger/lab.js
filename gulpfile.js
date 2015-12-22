185a63545a02c2cdbc9ed550dfa546742b2222ee
Felix Henninger <mailbox@felixhenninger.com>
2015-12-22 04:52:21 +0100

    Update banner to reflect library rename

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ const iife = require("gulp-iife");
 const header = require('gulp-header');
 
 const banner = [
-  '// jslab -- Building blocks for online experiments',
+  '// lab.js -- Building blocks for online experiments',
   '// (c) 2015- Felix Henninger',
   '\n'
   ].join('\n');
