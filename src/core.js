d1abe19e2272b8786c970bac76f8664703234790
Felix Henninger <mailbox@felixhenninger.com>
2016-01-25 15:38:04 +0100

    Add FormScreen for HTML form serialization and validation

--- src/core.js
+++ src/core.js
@@ -314,3 +314,117 @@ export class HTMLScreen extends BaseElement {
     return super.run()
   }
 }
+
+// A FormScreen can show, validate and serialize a form
+export class FormScreen extends HTMLScreen {
+  constructor(content, options={}) {
+    super(content, options)
+
+    // Add a validator
+    // (inactive by default)
+    this.validator = options.validator || (() => true)
+
+    // Capture form submissions
+    this.events['submit form'] = this.submit
+  }
+
+  submit(e=null) {
+    // Suppress default form behavior
+    if (e && e.preventDefault) {
+      e.preventDefault()
+    }
+
+    // Only continue if the
+    // form contents passes validation
+    if (this.validate()) {
+      // Add serialized form data to
+      // the element's dataset
+      this.data = _.extend(
+        this.data,
+        this.serialize()
+      )
+
+      // Bye!
+      this.end()
+    }
+
+    // Prevent default form behavior
+    return false
+  }
+
+  serialize() {
+    // Search for forms within the element
+    let forms = domSelect('form', this.el)
+
+    // Prepare an empty output object
+    let output = {}
+
+    // Iterate over forms ...
+    // (not that this slightly cumbersome detour
+    // is necessary because NodeLists, unlike arrays,
+    // do not support the forDach method)
+    Array.prototype.forEach.call(forms, (form) => {
+      // ... and elements within them
+      Array.prototype.forEach.call(form.elements, (element) => {
+        // Handle the element differently depending
+        // on the tag type
+        switch (element.nodeName.toLowerCase()) {
+          case 'input':
+            switch (element.type) {
+              case 'text':
+              case 'hidden':
+              case 'button':
+              case 'reset':
+              case 'submit':
+                output[element.name] = element.value
+                break
+              case 'checkbox':
+                output[element.name] = element.checked
+                break
+              case 'radio':
+                if (element.checked) {
+                  output[element.name] = element.value
+                }
+                break
+            }
+            break
+          case 'textarea':
+            output[element.name] = element.value
+            break
+          case 'select':
+            switch (element.type) {
+              case 'select-one':
+                output[element.name] = element.value
+                break
+              case 'select-multiple':
+                // Again, working around limitations of NodeLists
+                output[element.name] =
+                  Array.prototype.slice.call(element.options)
+                  .filter((option) => option.selected)
+                  .map((option) => option.value)
+                break
+            }
+            break
+          case 'button':
+            switch (element.type) {
+              case 'button':
+              case 'submit':
+              case 'reset':
+                output[element.name] = element.value
+            }
+            break
+        } // outer switch
+      }) // iterate across elements
+    }) // iterate across forms
+
+    return output
+  }
+
+  validate() {
+    // Validate the form by applying the
+    // form's validator to the serialized data
+    return this.validator(
+      this.serialize()
+    )
+  }
+}
