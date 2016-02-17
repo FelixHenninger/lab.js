bf9f68d7db618466573e93cf5829a82b84b07139
Felix Henninger <mailbox@felixhenninger.com>
2016-02-17 21:36:59 +0100

    Extract stages of canvas handling into separate functions

--- src/core-display-canvas.js
+++ src/core-display-canvas.js
@@ -3,27 +3,64 @@
 // Global canvas functions used in all of the following elements
 // (multiple inheritance would come in handy here, but alas...)
 
-// TODO: extract common functions
+// TODO: Rethink handling of the this binding
+// in the following code, and refactor if necessary.
+// (code is clean, but not necessarily as elegant
+// as possible)
+
+const canvas_init = function(options) {
+  // Setup canvas handling:
+  // By default, the element does not
+  // come bundled with a canvas. Instead,
+  // the expectation is that it will receive
+  // a canvas by the time it is prepared,
+  // otherwise the element will take care of
+  // creating its own canvas and appending
+  // it to the dom at runtime.
+  // Either way, a canvas is definitely present
+  // after the Element is prepared.
+  this.canvas = null
+  this.ctx_type = options.ctx_type || '2d'
+  this.ctx = null
+  this._canvas_needs_appending = false
+}
+
+const canvas_prepare = function() {
+  // Initialize a canvas,
+  // if this has not already been done
+  if (this.canvas == null) {
+    this.canvas = document.createElement('canvas')
+    // Remember to add the canvas to the DOM later
+    this._canvas_needs_appending = true
+  }
+}
+
+const canvas_insert = function() {
+  // Add the canvas to the DOM if need be
+  if (this._canvas_needs_appending) {
+    // Remove all other content within the HTML tag
+    // (note that this could be sped up, as per
+    // http://jsperf.com/innerhtml-vs-removechild
+    // it seems sufficient for the moment, though)
+    this.el.innerHTML = ''
+
+    // Adjust the canvas dimensions
+    // to match those of the containing element
+    this.canvas.width = this.el.clientWidth
+    this.canvas.height = this.el.clientHeight
+
+    // Append the canvas to the DOM
+    this.el.appendChild(this.canvas)
+  }
+}
 
 export class CanvasScreen extends BaseElement {
   constructor(render_function, options={}) {
     super(options)
     this.render_function = render_function
 
-    // Setup canvas handling:
-    // By default, the element does not
-    // come bundled with a canvas. Instead,
-    // the expectation is that it will receive
-    // a canvas by the time it is prepared,
-    // otherwise the element will take care of
-    // creating its own canvas and appending
-    // it to the dom at runtime.
-    // Either way, a canvas is definitely present
-    // after the Element is prepared.
-    this.canvas = null
-    this.ctx_type = options.ctx_type || '2d'
-    this.ctx = null
-    this._canvas_needs_appending = false
+    // Initialize canvas
+    canvas_init.apply(this, [options])
 
     // Provide an attribute for tracking
     // redraw requests
@@ -40,13 +77,7 @@ export class CanvasScreen extends BaseElement {
   }
 
   prepare() {
-    // Initialize a canvas,
-    // if this has not already been done
-    if (this.canvas == null) {
-      this.canvas = document.createElement('canvas')
-      // Remember to add the canvas to the DOM later
-      this._canvas_needs_appending = true
-    }
+    canvas_prepare.apply(this)
 
     // Bind render function to local context
     this.render_function = this.render_function.bind(this)
@@ -56,22 +87,8 @@ export class CanvasScreen extends BaseElement {
   }
 
   run() {
-    // Add the canvas to the DOM if need be
-    if (this._canvas_needs_appending) {
-      // Remove all other content within the HTML tag
-      // (note that this could be sped up, as per
-      // http://jsperf.com/innerhtml-vs-removechild
-      // it seems sufficient for the moment, though)
-      this.el.innerHTML = ''
-
-      // Adjust the canvas dimensions
-      // to match those of the containing element
-      this.canvas.width = this.el.clientWidth
-      this.canvas.height = this.el.clientHeight
-
-      // Append the canvas to the DOM
-      this.el.appendChild(this.canvas)
-    }
+    // Add canvas to the dom, if necessary
+    canvas_insert.apply(this)
 
     // Extract the requested context for the canvas
     this.ctx = this.canvas.getContext(
