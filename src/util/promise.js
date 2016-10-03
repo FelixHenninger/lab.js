ac0d964b9cc36b5416abb36c47244948b1841f27
Felix Henninger <mailbox@felixhenninger.com>
2016-10-03 19:34:15 +0200

    Remove now-unnecessary promise helpers

--- src/util/promise.js
+++
@@ -1,7 +0,0 @@
-// Given an array of functions that return promises,
-// execute them in sequence with specified arguments
-export const promiseChain = tasks =>
-  tasks.reduce(
-    (chain, f) => chain.then(f),
-    Promise.resolve()
-  )
