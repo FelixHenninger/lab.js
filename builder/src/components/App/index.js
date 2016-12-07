6157574370a94697c93f18c60be80acf6de02c12
Felix Henninger <mailbox@felixhenninger.com>
2016-12-07 21:41:24 +0100

    Add editor card

--- builder/src/components/App/index.js
+++ builder/src/components/App/index.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import Layout from '../Layout'
 import Card from '../Card'
+import MonacoCard from '../MonacoCard'
 
 class App extends Component {
   render() {
     return (
       <Layout sidebar={ <div>Sidebar</div> }>
         <Card title="Card">Content</Card>
+        <MonacoCard title="Editor"/>
       </Layout>
     )
   }
---
+++ builder/src/components/MonacoCard/index.js
@@ -0,0 +1,41 @@
+import React from 'react'
+
+import Card from '../Card'
+import MonacoEditor from 'react-monaco-editor'
+import './editor-style-overrides.css'
+
+// TODO: I haven't found a way to reliably host the
+// monaco static files on another path than /vs .
+// It would be nice to be able to move it into the
+// /vendor directory
+
+export default (props) => {
+  const requireConfig = {
+    url: 'https://cdnjs.cloudflare.com/ajax/libs/require.js/2.3.1/require.min.js',
+    paths: {
+      'vs': process.env.PUBLIC_URL + '/vs'
+    }
+  };
+  return <Card title={props.title}>
+    <MonacoEditor
+      width="100%" height="600"
+      language="html" value={''}
+      requireConfig={requireConfig}
+      options={{
+        // Behavior
+        contextmenu: false,
+        lineNumbers: true,
+        lineNumbersMinChars: 4,
+        overviewRulerLanes: 0,
+        rulers: [80],
+        scrollBeyondLastLine: false,
+        wrappingColumn: 0,
+        // Style
+        fontFamily: 'Fira Mono',
+        fontSize: 18,
+        lineHeight: 26,
+      }}
+      {...props}
+    />
+  </Card>
+}
