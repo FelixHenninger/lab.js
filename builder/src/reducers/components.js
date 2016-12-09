2cfeab400dd88dcac8981a6d192c31a3c40a3005
Felix Henninger <mailbox@felixhenninger.com>
2016-12-09 18:06:56 +0100

    Add main reducer for component-related actions

--- builder/src/reducers/components.js
+++ builder/src/reducers/components.js
@@ -1,5 +1,8 @@
+// Define a default app state
+// TODO: Think about removing the id property
+// on the components; it's superfluous, but
+// might be handy in the app
 const defaultState = {
-  // root
   'root': {
     id: 'root',
     title: 'root',
@@ -20,5 +23,128 @@ const defaultState = {
   },
 }
 
-export default (state=defaultState, action) =>
-  state
+// Provide unique ids for individual components
+let idCounter = 0
+const uniqueId = function(state={}) {
+  // Collect previously used ids
+  const usedIds = Object
+    .entries(state)
+    .map( ([k, v]) => k )
+
+  // Generate as yet unused candidate
+  let candidate
+  do {
+    candidate = (++idCounter).toString()
+  } while (usedIds.includes(candidate))
+
+  return candidate
+}
+
+// Define reducers that operate
+// on the main study representation
+
+// TODO: Refactor this so that the children
+// arrays are handled by their own reducers,
+// making this code more legible
+export default (state=defaultState, action) => {
+  switch (action.type) {
+    case 'MOVE_COMPONENT':
+      if (action.oldParent === action.newParent) {
+        // Remove component from old position
+        const childrenStepOne = [
+          ...state[action.newParent].children.slice(0, action.oldIndex),
+          ...state[action.newParent].children.slice(action.oldIndex + 1)
+        ]
+
+        // Add component in new position
+        const childrenStepTwo = [
+          ...childrenStepOne.slice(0, action.newIndex),
+          action.id,
+          ...childrenStepOne.slice(action.newIndex),
+        ]
+
+        return {
+          ...state,
+          [action.newParent]: {
+            ...state[action.newParent],
+            children: childrenStepTwo
+          }
+        }
+      } else {
+        return {
+          ...state,
+          // Add component in new position
+          [action.newParent]: {
+            ...state[action.newParent],
+            children: !state[action.newParent].children ? [action.id] : [
+              ...state[action.newParent].children.slice(0, action.newIndex),
+              action.id,
+              ...state[action.newParent].children.slice(action.newIndex),
+            ]
+          },
+          // Remove component from old position
+          [action.oldParent]: {
+            ...state[action.oldParent],
+            children: [
+              ...state[action.oldParent].children.slice(0, action.oldIndex),
+              ...state[action.oldParent].children.slice(action.oldIndex + 1),
+            ]
+          },
+        }
+      }
+    case 'ADD_COMPONENT':
+      const componentData = {
+        id: action.id || uniqueId(state),
+        ...action.data,
+      }
+      return {
+        ...state,
+        [componentData.id]: componentData,
+        [action.parent]: {
+          ...state[action.parent],
+          children: !state[action.parent].children ? [componentData.id] : [
+            ...state[action.parent].children.slice(0, action.index),
+            componentData.id,
+            ...state[action.parent].children.slice(action.index),
+          ]
+        }
+      }
+    case 'DELETE_COMPONENT':
+      // TODO: This doesn't account for the possibility
+      // that the same component might be duplicated
+      // within the same parent component -- but that's
+      // pretty much hypothetical for now.
+      // Solving this will require passing the index
+      // along with the parent
+      const componentInUse = Object.entries(state)
+        .filter(([id, c]) => c.children && c.children.includes(action.id))
+        .length > 1
+
+      const output = {
+        ...state,
+        [action.parent]: {
+          ...state[action.parent],
+          children: state[action.parent].children.filter(x => x !== action.id)
+        }
+      }
+
+      // If it is not used elsewhere, remove
+      // the component from the state object.
+      if (!componentInUse) {
+        delete output[action.id]
+      }
+
+      return output
+    case 'UPDATE_COMPONENT':
+      return {
+        ...state,
+        [action.id]: {
+          ...state[action.id],
+          ...action.data,
+        }
+      }
+
+    default:
+      return state
+  }
+}
---
+++ builder/src/reducers/components.test.js
@@ -0,0 +1,154 @@
+import updateComponent from './components.js'
+
+const initialState = {
+  root: {
+    children: ['A', 'B', 'C']
+  },
+  A: {},
+  B: {},
+  C: {},
+}
+
+it('adds a component at a certain position', () => {
+  expect(updateComponent(
+    initialState,
+    {
+      type: 'ADD_COMPONENT',
+      id: 'E',
+      parent: 'root',
+      index: 2,
+      data: {
+        foo: 'bar',
+      },
+    }
+  )).toEqual(
+    {
+      ...initialState,
+      root: {
+        children: ['A', 'B', 'E', 'C'],
+      },
+      E: {
+        foo: 'bar',
+        id: 'E',
+      }
+    }
+  )
+})
+
+it('deletes a component that occurs only a single time', () => {
+  expect(updateComponent(
+    initialState,
+    {
+      type: 'DELETE_COMPONENT',
+      id: 'B',
+      parent: 'root',
+    }
+  )).toEqual(
+    {
+      root: {
+        ...initialState.root,
+        children: ['A', 'C'],
+      },
+      A: initialState.A,
+      C: initialState.C,
+    }
+  )
+})
+
+it('removes only the reference if a component occurs more than once', () => {
+  const state = {
+    root: {
+      children: ['A', 'B'],
+    },
+    A: {
+      children: ['C'],
+    },
+    B: {
+      children: ['C'],
+    },
+    C: {},
+  }
+  expect(updateComponent(
+    state,
+    {
+      type: 'DELETE_COMPONENT',
+      id: 'C',
+      parent: 'A',
+    }
+  )).toEqual(
+    {
+      root: state.root,
+      A: {
+        children: [],
+      },
+      B: state.B,
+      C: state.C,
+    }
+  )
+})
+
+it('moves a component between parents (when destination has no children)', () => {
+  expect(updateComponent(
+    initialState,
+    {
+      type: 'MOVE_COMPONENT',
+      id: 'B',
+      oldParent: 'root',
+      oldIndex: 1,
+      newParent: 'C',
+      newIndex: 0,
+    }
+  )).toEqual(
+    {
+      ...initialState,
+      root: {
+        children: ['A', 'C'],
+      },
+      C: {
+        children: ['B'],
+      }
+    }
+  )
+})
+
+it('moves a component forwards within a parent component', () => {
+  expect(updateComponent(
+    initialState,
+    {
+      type: 'MOVE_COMPONENT',
+      id: 'B',
+      oldParent: 'root',
+      oldIndex: 1,
+      newParent: 'root',
+      newIndex: 2,
+    }
+  )).toEqual(
+    {
+      ...initialState,
+      root: {
+        children: ['A', 'C', 'B'],
+      },
+    }
+  )
+})
+
+it('moves a component backwards within a parent component', () => {
+  expect(updateComponent(
+    initialState,
+    {
+      type: 'MOVE_COMPONENT',
+      id: 'B',
+      oldParent: 'root',
+      oldIndex: 1,
+      newParent: 'root',
+      newIndex: 0,
+    }
+  )).toEqual(
+    {
+      ...initialState,
+      root: {
+        children: ['B', 'A', 'C'],
+      },
+    }
+  )
+})
