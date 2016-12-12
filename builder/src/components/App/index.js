125760104e16274a01c3301068342cbdb77cb24b
Felix Henninger <mailbox@felixhenninger.com>
2016-12-12 16:40:30 +0100

    Add hint component for in-UI documentation

--- builder/src/components/App/index.js
+++ builder/src/components/App/index.js
@@ -2,12 +2,18 @@ import React, { Component } from 'react'
 import Layout from '../Layout'
 import Card from '../Card'
 import MonacoCard from '../MonacoCard'
+import Hint from '../Hint'
 
 class App extends Component {
   render() {
     return (
       <Layout sidebar={ <div>Sidebar</div> }>
-        <Card title="Card">Content</Card>
+        <Card title="Card">
+          Content
+          <Hint id="hint-test" title="This is a hint" className="pull-right">
+            This is some content. Woo!
+          </Hint>
+        </Card>
         <MonacoCard title="Editor"/>
       </Layout>
     )
---
+++ builder/src/components/Hint/index.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import { Popover, PopoverTitle, PopoverContent } from 'reactstrap'
+import { uniqueId } from 'lodash'
+
+import './hint.css'
+
+export default class HintPopover extends Component {
+  constructor(props) {
+    super(props)
+
+    this.id = this.props.id || uniqueId('hint_')
+
+    this.toggle = this.toggle.bind(this)
+    this.state = {
+      isOpen: false,
+    }
+  }
+
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen,
+    })
+  }
+
+  render() {
+    return (
+      <span className={ this.props.className } style={ this.props.style }>
+        {' '}
+        <span id={ this.id } onClick={ this.toggle }>
+          <i className="fa fa-info-circle" style={{ color: 'rgba(0, 0, 0, 0.2)' }}
+            aria-hidden="true" title="More information"></i>
+          <span className="sr-only">More information</span>
+        </span>
+        <Popover
+          target={ this.id }
+          placement={ this.props.placement || "bottom" }
+          isOpen={ this.state.isOpen } toggle={ this.toggle }>
+          <PopoverTitle>{ this.props.title }</PopoverTitle>
+          <PopoverContent>{ this.props.children }</PopoverContent>
+        </Popover>
+      </span>
+    )
+  }
+}
