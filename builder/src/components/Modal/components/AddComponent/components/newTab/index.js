fe01ff02ebf011e2bd444de86560ac7280d0a374
Felix Henninger <mailbox@felixhenninger.com>
2017-01-28 17:13:57 +0100

    Extract AddComponent modal content into separate module

---
+++ builder/src/components/Modal/components/AddComponent/components/newTab/index.js
@@ -0,0 +1,60 @@
+import React from 'react'
+
+import { Card, CardBlock, CardGroup, CardHeader } from 'reactstrap'
+import { metadata } from '../../../../../../logic/components'
+
+import './style.css'
+
+const ComponentDisplay = ({ value, parent, index }, context) =>
+  <Card
+    className="add-component-card"
+    onClick={ () => {
+      context.store.dispatch({
+        type: 'ADD_COMPONENT',
+        parent, index,
+        data: {
+          ...value.defaults,
+          title: value.name,
+        },
+      })
+      // TODO: Show the new component
+      context.store.dispatch({
+        type: 'HIDE_MODAL',
+      })
+    }}
+  >
+    <CardHeader
+      style={{
+        textAlign: 'center',
+      }}
+    >
+      <i
+        className={`fa fa-${ value.icon }`}
+        style={{
+          fontSize: '2rem',
+        }}
+      ></i>
+    </CardHeader>
+    <CardBlock>
+      <h5 className="card-title">{ value.name }</h5>
+      <h6 className="card-subtitle mb-2 text-muted">{ value.category }</h6>
+    </CardBlock>
+  </Card>
+
+ComponentDisplay.contextTypes = {
+  store: React.PropTypes.object,
+}
+
+export default ({ parent, index }) =>
+  <CardGroup>
+    {
+      Object.entries(metadata).map(([key, value]) =>
+        <ComponentDisplay
+          key={ key }
+          value={ value }
+          parent={ parent }
+          index={ index }
+        />
+      )
+    }
+  </CardGroup>
--- builder/src/components/Modal/components/AddComponent/index.js
+++ builder/src/components/Modal/components/AddComponent/index.js
@@ -1,48 +1,7 @@
 import React from 'react'
-import { Button, Card, CardBlock, CardHeader, ModalHeader, ModalBody, ModalFooter } from 'reactstrap'
+import { Button, ModalHeader, ModalBody, ModalFooter } from 'reactstrap'
 
-import { metadata } from '../../../../logic/components'
-import './style.css'
-
-const ComponentDisplay = ({ value, parent, index }, context) =>
-  <Card
-    className="add-component-card"
-    onClick={ () => {
-      context.store.dispatch({
-        type: 'ADD_COMPONENT',
-        parent, index,
-        data: {
-          ...value.defaults,
-          title: value.name,
-        },
-      })
-      // TODO: Show the new component
-      context.store.dispatch({
-        type: 'HIDE_MODAL',
-      })
-    }}
-  >
-    <CardHeader
-      style={{
-        textAlign: 'center',
-      }}
-    >
-      <i
-        className={`fa fa-${ value.icon }`}
-        style={{
-          fontSize: '2rem',
-        }}
-      ></i>
-    </CardHeader>
-    <CardBlock>
-      <h5 className="card-title">{ value.name }</h5>
-      <h6 className="card-subtitle mb-2 text-muted">{ value.category }</h6>
-    </CardBlock>
-  </Card>
-
-ComponentDisplay.contextTypes = {
-  store: React.PropTypes.object,
-}
+import NewTab from './components/newTab'
 
 const AddComponentModal = ({ parent, index, closeHandler }, context) =>
   <div className="modal-content">
@@ -55,18 +14,10 @@ const AddComponentModal = ({ parent, index, closeHandler }, context) =>
       </small>
     </ModalHeader>
     <ModalBody>
-      <div className="card-group">
-        {
-          Object.entries(metadata).map(([key, value]) =>
-            <ComponentDisplay
-              key={ key }
-              value={ value }
-              parent={ parent }
-              index={ index }
-            />
-          )
-        }
-      </div>
+      <NewTab
+        parent={ parent }
+        index={ index }
+      />
     </ModalBody>
     <ModalFooter>
       <Button
