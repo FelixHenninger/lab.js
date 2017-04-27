227e16c4827cb046b5579a7bc2e15b20cb8eec04
Felix Henninger <mailbox@felixhenninger.com>
2017-04-27 18:46:52 +0200

    Slightly clean up worker initialization code

--- builder/src/logic/preview/worker.js
+++ builder/src/logic/preview/worker.js
@@ -11,16 +11,16 @@ export default (store) => {
     // Register service worker
     navigator.serviceWorker
       // Remove legacy workers during architectural transition
-      .getRegistrations()
-      .then( registrations => Promise.all(
-        registrations.map( r => r.unregister() )
-      ))
-      // Setup new worker
-      .then( () =>
+      .getRegistrations().then(registrations =>
+        Promise.all(
+          registrations.map( r => r.unregister() )
+        )
+      ).then(() =>
+        // Setup new worker
         navigator.serviceWorker.register('api/worker.js', { scope: rootUrl })
-      )
-      .then(() => console.log(`Service worker registered successfully at ${ rootUrl }`))
-      .catch(e => {
+      ).then(() =>
+        console.log(`Service worker registered successfully at ${ rootUrl }`)
+      ).catch(e => {
         console.log('Error during service worker registration:', e)
         Raven.captureException(e)
       })
