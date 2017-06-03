c3cf1a785ed7787b930349ed7d0c35e1fd69c71a
Felix Henninger <mailbox@felixhenninger.com>
2017-06-03 14:02:32 +0200

    Fix failing test checking app initialization
    It turns out that the test was missing a lot of context logic required by the <App /> component and was failing because, for example, the store was not passed down to nested components. The redux documentation on testing (http://redux.js.org/docs/recipes/WritingTests.html#connected-components) finally provided the necessary hint.
    
    This test is somewhat useful, in that it checks whether the app can be loaded in a jsdom environment at all. However, because it doesn't test functionality and isn't very specific, its use is limited (problems it uncovers will probably pop up during the build stage). It might therefore be removed at some later point.

--- builder/src/components/App/index.test.js
+++ builder/src/components/App/index.test.js
@@ -2,7 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './'
 
+import { Provider } from 'react-redux'
+import store from '../../store'
+
+import { DragDropContext } from 'react-dnd'
+import HTML5DragDropBackend from 'react-dnd-html5-backend'
+
+// A fair amount of boilerplate code is necessary here
+// to establish a context in which the app can run.
 it('renders without crashing', () => {
+  const WrappedApp = DragDropContext(HTML5DragDropBackend)(App)
   const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
+  ReactDOM.render(
+    <Provider store={ store }>
+      <WrappedApp />
+    </Provider>,
+    div
+  )
 })
