ed44983b40586b87213e2d3a7ef07359e1981715
Felix Henninger <mailbox@felixhenninger.com>
2017-07-06 18:15:24 +0200

    Add tests for plugins

--- packages/library/test/plugins.js
+++ packages/library/test/plugins.js
@@ -1,8 +1,35 @@
-/* global describe, it, assert */
+/* global describe, it, sinon, assert, beforeEach */
 
 define(['lab'], (lab) => {
 
 describe('Plugins', () => {
+  describe('Logger', () => {
+    let fakeLog
+    beforeEach(() => {
+      fakeLog = sinon.stub(console, 'log').returns(undefined)
+    })
+
+    afterEach(() => {
+      console.log.restore()
+    })
+
+    it('logs events to console', () => {
+      const c = new lab.core.Component({
+        plugins: [
+          new lab.plugins.Logger({ title: 'FancyComponent' }),
+        ],
+      })
+
+      c.trigger('SomeEvent')
+
+      assert.ok(
+        fakeLog
+          .withArgs('Component FancyComponent received SomeEvent')
+          .calledOnce
+      )
+    })
+  })
+
   describe('Metadata', () => {
     it('saves metadata to component during preparation', () => {
       const c = new lab.core.Component({
@@ -31,6 +58,56 @@ describe('Plugins', () => {
         assert.include(
           c.options.datastore.staging.meta,
           { userAgent: window.navigator.userAgent },
+        )
+      })
+    })
+  })
+
+  describe('Transmit', () => {
+
+    let c, p
+    beforeEach(() => {
+      p = new lab.plugins.Transmit({
+        url: 'https://arbitrary.example',
+      })
+      c = new lab.core.Component({
+        datastore: new lab.data.Store(),
+        plugins: [ p ],
+      })
+
+      sinon.stub(c.options.datastore, 'transmit')
+    })
+    afterEach(() => {
+      c.options.datastore.transmit.restore()
+    })
+
+    it('sends state when data is committed', () => {
+      return c.prepare().then(() => {
+        c.options.datastore.commit({ a: 1 })
+
+        assert.ok(
+          c.options.datastore.transmit.withArgs(
+            'https://arbitrary.example',
+            { id: p.metadata.id },
+            'staging',
+          ).calledOnce
+        )
+      })
+    })
+
+    it('sends complete dataset when component ends', () => {
+      return c.run().then(() => {
+        // Data transmission runs last,
+        // so we need to wait for the corresponding event
+        const promise = c.waitFor('after:end')
+        c.end()
+        return promise
+      }).then(() => {
+        assert.ok(
+          c.options.datastore.transmit.withArgs(
+            'https://arbitrary.example',
+            { id: p.metadata.id },
+          ).calledOnce
         )
       })
     })
