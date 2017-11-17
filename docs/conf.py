eeefe3e54ddeee8ef3ee0de79727df3db16b806b
Felix Henninger <mailbox@felixhenninger.com>
2017-11-17 12:17:25 +0100

    Add reST extension for embedding video

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ import shlex
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('_extensions'))
 
 # Sphinx Theme by Read The Docs
 import sphinx_rtd_theme
@@ -33,7 +33,9 @@ import sphinx_rtd_theme
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = [
+    'rst_video'
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
