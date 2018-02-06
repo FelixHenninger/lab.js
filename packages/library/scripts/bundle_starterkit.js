5f9f547e5d371e468fcf7d2c9acb7525998690f8
Felix Henninger <mailbox@felixhenninger.com>
2018-02-06 22:29:55 +0100

    Enable library builds outside a unix shell environment

---
+++ packages/library/scripts/bundle_starterkit.js
@@ -0,0 +1,35 @@
+const shell = require('shelljs')
+
+// Copy files ------------------------------------------------------------------
+shell.mkdir('-p', 'dist/labjs-starterkit/lib')
+shell.cp('-R', 'src/starterkit/index.html', 'dist/labjs-starterkit')
+shell.cp('-R', 'src/starterkit/experiment.js', 'dist/labjs-starterkit')
+shell.cp('-R', 'dist/lab.dev.js', 'dist/labjs-starterkit/lib')
+shell.cp('-R', 'dist/lab.js', 'dist/labjs-starterkit/lib')
+shell.cp('-R', 'dist/lab.js.map', 'dist/labjs-starterkit/lib')
+shell.cp('-R', 'dist/lab.css', 'dist/labjs-starterkit/lib')
+shell.cp('-R', 'src/starterkit/lib/loading.svg', 'dist/labjs-starterkit/lib')
+
+// Create the starterkit bundle ------------------------------------------------
+// (TODO: Think about doing this directly from the source files)
+const JSZip = require('jszip')
+const fs = require('fs')
+zip = new JSZip()
+
+shell.ls('-R', 'dist/labjs-starterkit/**/*')
+  .filter(filename => filename.startsWith('dist/labjs-starterkit'))
+  .forEach(filename => {
+    zip.file(
+      filename.replace(/^dist\/labjs-starterkit\//, ''),
+      fs.readFileSync(filename)
+    )
+  })
+
+// Compress and output bundle file
+zip
+  .generateNodeStream({
+    compression: 'DEFLATE', compressionOptions: { level: 9 },
+    type:'nodebuffer', streamFiles:true,
+  })
+  .pipe(fs.createWriteStream('dist/labjs-starterkit.zip'))
+  .on('finish', () => console.log('Created starterkit bundle'))
