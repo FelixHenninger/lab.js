1f62f88decaeb0726243ac9f5512093ef6bcce24
Felix Henninger <mailbox@felixhenninger.com>
2018-02-20 00:08:55 +0100

    Extract canvas-based editor into separate chunk

--- packages/builder/src/components/ComponentOptions/components/Content/Canvas/index.js
+++ packages/builder/src/components/ComponentOptions/components/Content/Canvas/index.js
@@ -1,14 +1,22 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Loadable from 'react-loadable'
 
 import Card from '../../../../Card'
-import CanvasEditor from './editor'
-
 import { updateComponent } from '../../../../../actions/components'
+
+const LoadableEditor = Loadable({
+  loader: () => import('./editor'),
+  loading() {
+    return <div className="text-center">
+      <i className="fas fa-spinner-third fa-spin my-3" />
+    </div>
+  }
+})
 
 const Editor = ({ id, data }, { store }) =>
   <Card title="Content">
-    <CanvasEditor
+    <LoadableEditor
       data={ data.content }
       onChange={
         newData => store.dispatch(
