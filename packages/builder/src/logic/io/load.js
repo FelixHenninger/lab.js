6b5bac203719a8e65c1659080cf3669e7481a5d1
Felix Henninger <mailbox@felixhenninger.com>
2018-04-04 15:44:09 +0200

    Add version to study state if not already present

--- packages/builder/src/logic/io/load.js
+++ packages/builder/src/logic/io/load.js
@@ -1,7 +1,15 @@
 import upgrade from './upgrade'
 
 export const fromObject = o =>
-  upgrade(o)
+  upgrade({
+    // Inject an early fallback version,
+    // in case the data doesn't include one
+    // already.
+    // TODO: Remove this after versioning
+    // has been firmly established.
+    version: [2017, 1, 7],
+    ...o,
+  })
 
 export const fromJSON = input =>
   fromObject(
