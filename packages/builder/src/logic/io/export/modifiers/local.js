c2e63b237eb76009aba0e78f32d18909d3075918
Felix Henninger <mailbox@felixhenninger.com>
2018-04-11 21:59:11 +0200

    Use smart filenames for downloaded csv files
    Thanks to Emily Elliott for suggesting this feature!
    
    Co-authored-by: lsuemily <32335879+lsuemily@users.noreply.github.com>

--- packages/builder/src/logic/io/export/modifiers/local.js
+++ packages/builder/src/logic/io/export/modifiers/local.js
@@ -1,10 +1,14 @@
 import { exportStatic } from '../index'
+import { makeFilename } from '../../filename'
 
 export const addDownloadPlugin = (state) => {
   // Add download plugin to root component
   state.components.root.plugins = [
     ...state.components.root.plugins,
-    { type: 'lab.plugins.Download' },
+    {
+      type: 'lab.plugins.Download',
+      filePrefix: makeFilename(state),
+    },
   ]
 
   return state
--- packages/library/src/plugins/download.js
+++ packages/library/src/plugins/download.js
@@ -1,3 +1,10 @@
+import { padStart } from 'lodash'
+
+// TODO: Replace lodash function with
+// String.prototype.padStart as soon as
+// browser compatibility allows.
+const twoDigit = x => padStart(x, 2, '0')
+
 const unloadHandler = (e) => {
   const warning = 'Are you sure you want to close this window?'
   e.returnValue = warning
@@ -5,8 +12,22 @@ const unloadHandler = (e) => {
 }
 
 export default class Download {
-  constructor() {
+  constructor({ filePrefix, fileType }) {
     this.el = null
+    this.filePrefix = filePrefix || 'study'
+    this.fileType = fileType || 'csv'
+  }
+
+  get filename() {
+    const d = new Date()
+
+    const date =
+      `${ d.getFullYear() }-` +
+      `${ twoDigit((d.getMonth() + 1).toString()) }-` +
+      `${ twoDigit(d.getDate().toString()) }--` +
+      `${ d.toTimeString().split(' ')[0] }`
+
+    return `${ this.filePrefix }-${ date }.${ this.fileType }`
   }
 
   handle(context, event) {
@@ -25,7 +46,7 @@ export default class Download {
       this.el.addEventListener(
         'click',
         () => {
-          context.options.datastore.download()
+          context.options.datastore.download(this.fileType, this.filename)
           window.removeEventListener('beforeunload', unloadHandler)
         },
       )
