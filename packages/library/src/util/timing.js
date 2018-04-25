38542a12837fa37b277160b5f2ff0b451472c940
Felix Henninger <mailbox@felixhenninger.com>
2018-04-25 23:41:03 +0200

    Add alternate timing methods

--- packages/library/src/util/timing.js
+++ packages/library/src/util/timing.js
@@ -51,3 +51,71 @@ export class StackTimeout {
     window.clearTimeout(this._timeoutHandle)
   }
 }
+
+const thresholds = {
+  'overshoot': 1,
+  'closest': 1.5,
+  'undershoot': 2,
+}
+
+export class FrameTimeout {
+  constructor(f, delay, ...params) {
+    this.delay = delay
+    this.f = f
+    this.params = params
+
+    // Internal state
+    this._running = false
+    this._timeoutHandle = undefined
+    this._animationFrameHandle = undefined
+    this._lastAnimationFrame = undefined
+
+    // Timing mode
+    this.mode = 'closest'
+
+    // Bind tick method
+    this.tick = this.tick.bind(this)
+  }
+
+  tick(frameTime=performance.now()) {
+    // This is a rough approximation -- we can't assume that every
+    // monitor is running at 60Hz, but the interval between animation
+    // frames is also an imperfect proxy for the monitor's refresh time
+    const frameDuration = (frameTime - this._lastAnimationFrame) || 16.66
+
+    // Calculate the remaining time in frames
+    const remainingFrames = (this.targetTime - frameTime) / frameDuration
+
+    if (remainingFrames <= thresholds[this.mode]) {
+      // Fire callback
+      this.f(frameTime, ...this.params)
+    } else if (this.targetTime - frameTime < 200) {
+      // Check again on the next frame
+      this._animationFrameHandle = window.requestAnimationFrame(this.tick)
+      this._lastAnimationFrame = frameTime
+    } else {
+      // Wait a little longer
+      // (to be exact, wait for half of the remaining time, minus 100ms)
+      this._timeoutHandle = window.setTimeout(
+        this.tick,
+        (this.targetTime - frameTime) / 2 - 100
+      )
+    }
+  }
+
+  run() {
+    if (!this._running) {
+      // Set target time in milliseconds
+      this.targetTime = performance.now() + this.delay
+      this.tick()
+      this._running = true
+    } else {
+      console.log('Cannot restart previously run timer')
+    }
+  }
+
+  cancel() {
+    window.clearTimeout(this._timeoutHandle)
+    window.cancelAnimationFrame(this._animationFrameHandle)
+  }
+}
