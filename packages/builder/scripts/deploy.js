49a043c8b4819807063753f443fc2d96fcf6963d
Felix Henninger <mailbox@felixhenninger.com>
2018-07-21 22:23:08 +0200

    Only deploy commits on the master branch

--- packages/builder/scripts/deploy.js
+++ packages/builder/scripts/deploy.js
@@ -1,9 +1,17 @@
 const shell = require('shelljs')
+const { CI, TRAVIS_PULL_REQUEST, TRAVIS_BRANCH } = process.env
 
-shell.exec('cd build && zip -r ../dist.zip .')
-shell.exec('curl ' +
-  '-H "Content-Type: application/zip" ' +
-  '-H "Authorization: Bearer $NETLIFY_ACCESS_TOKEN" ' +
-  '--data-binary "@dist.zip" ' +
-  'https://api.netlify.com/api/v1/sites/$NETLIFY_SITE_ID/deploys'
-)
+if (!(
+  CI === 'true' && (
+    TRAVIS_BRANCH !== 'master' ||
+    TRAVIS_PULL_REQUEST === 'true'
+  )
+)) {
+  shell.exec('cd build && zip -r ../dist.zip .')
+  shell.exec('curl ' +
+    '-H "Content-Type: application/zip" ' +
+    '-H "Authorization: Bearer $NETLIFY_ACCESS_TOKEN" ' +
+    '--data-binary "@dist.zip" ' +
+    'https://api.netlify.com/api/v1/sites/$NETLIFY_SITE_ID/deploys'
+  )
+}
