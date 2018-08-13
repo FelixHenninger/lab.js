3f228ed5442ef5cce1b1416a2ceb80f477882d22
Felix Henninger <mailbox@felixhenninger.com>
2018-08-13 15:18:41 +0200

    Check whether the browser provides extra-persistent storage

--- packages/builder/src/logic/util/persistence.js
+++ packages/builder/src/logic/util/persistence.js
@@ -5,7 +5,7 @@ const lf = localForage.createInstance({
   name: "lab.js"
 })
 
-export const persistState = async store =>
+export const persistState = async store => {
   // Persist application state to localForage on changes
   store.subscribe(() => {
     lf.setItem(
@@ -13,6 +13,20 @@ export const persistState = async store =>
       store.getState()
     )
   })
+
+  // Check for persistence of in-browser storage
+  if (navigator.storage && navigator.storage.persist) {
+    navigator.storage.persisted().then(persistent => {
+      if (persistent) {
+        console.log('Persistent storage enabled')
+      } else {
+        console.log('Persistent storage not permitted');
+      }
+    })
+  } else {
+    console.log('No support for persistent storage')
+  }
+}
 
 export const retrieveState = async () => {
   // Migrate storage from localStorage to localForage/IDB
