e812f0a3dda9ed8790e9de628784545a8ad7a4e2
Felix Henninger <mailbox@felixhenninger.com>
2018-08-27 17:12:59 +0200

    Support loading remote studies via URL parameter

---
+++ packages/builder/src/logic/util/remoteStudy.js
@@ -0,0 +1,46 @@
+import { fromObject } from '../io/load'
+
+// Load a study from a remote URL
+export const fromURL = async () => {
+  // Extract study URL from current location
+  const location = new URL(window.location.href)
+
+  // Stop during automated testing
+  if (!location || !location.searchParams) {
+    return
+  }
+
+  // Extract url parameter from path
+  const url = location.searchParams.get('url')
+
+  // Make sure URL is present and the user knows what is happening
+  if (
+    url &&
+    window.confirm(`Would you like to load the study from ${ url }?`)
+  ) {
+    let response
+    try {
+      response = await fetch(url)
+    } catch(e) {
+      alert(`Connection error while trying to load remote study: ${ e }`)
+      return
+    }
+
+    // Catch 404 responses, etc.
+    if (!response.ok) {
+      alert(`Invalid response while loading remote study, code ${ response.status }: ${ response.statusText }`)
+      return
+    }
+
+    // Parse JSON
+    let data
+    try {
+      data = await response.json()
+    } catch(e) {
+      alert(`Couldn't decode study data: ${ e }`)
+      return
+    }
+
+    return fromObject(data)
+  }
+}
--- packages/builder/src/store.js
+++ packages/builder/src/store.js
@@ -2,10 +2,11 @@ import { createStore } from 'redux'
 import reducer from './reducers'
 
 import { retrieveState } from './logic/util/persistence'
+import { fromURL } from './logic/util/remoteStudy';
 
 const configureStore = async initialState =>
   createStore(
-    reducer, initialState || await retrieveState(),
+    reducer, initialState || await fromURL() || await retrieveState(),
     window.devToolsExtension && window.devToolsExtension(),
   )
 
