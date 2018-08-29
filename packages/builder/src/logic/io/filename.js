4f496b3badbe022982c1b47e4ae9f3c82c1a6bd8
Felix Henninger <mailbox@felixhenninger.com>
2018-08-29 19:42:48 +0200

    Stabilize filename generation

--- packages/builder/src/logic/io/filename.js
+++ packages/builder/src/logic/io/filename.js
@@ -4,4 +4,10 @@ export const slugify = title =>
   defaultSlugify(title).toLowerCase()
 
 export const makeFilename = state =>
-  slugify(state.components['root'].metadata.title.trim() || 'study')
+  // TODO: Simplify this once optional chaining becomes available
+  slugify((
+    state.components['root'] &&
+    state.components['root'].metadata &&
+    state.components['root'].metadata.title &&
+    state.components['root'].metadata.title.trim()
+  ) || 'study')
