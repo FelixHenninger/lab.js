5ca50a9076b33e49dbefda2740df4960addb0111
Felix Henninger <mailbox@felixhenninger.com>
2018-10-16 10:44:46 +0200

    Extract file import logic into its own function

--- packages/builder/src/components/Modal/components/AddComponent/components/importTab/index.js
+++ packages/builder/src/components/Modal/components/AddComponent/components/importTab/index.js
@@ -3,6 +3,57 @@ import PropTypes from 'prop-types'
 import Uploader from '../../../../../Uploader'
 import { fromJSON } from '../../../../../../logic/io/load'
 
+export const importComponent = (parent, index, state, store) => {
+  // Hydrate store from resulting object
+  if (state.components.root.children.length === 1) {
+    // If the root component has only a single child,
+    // import that.
+    store.dispatch({
+      type: 'IMPORT_COMPONENT',
+      parent, index,
+      id: state.components.root.children[0],
+      source: state.components,
+    })
+  } else {
+    // If the root component has multiple descendants,
+    // import them as a sequence
+
+    // Remove metadata collection plugin
+    // (which is automatically added to every study root)
+    state.components.root.plugins =
+      state.components.root.plugins.filter(
+        x => x.type !== 'lab.plugins.Metadata'
+      )
+
+    // Rename task from metadata
+    state.components.root.title =
+      state.components.root.metadata.title || 'Unnamed task'
+
+    store.dispatch({
+      type: 'IMPORT_COMPONENT',
+      parent, index,
+      id: 'root',
+      source: state.components,
+    })
+  }
+
+  // Merge files
+  store.dispatch({
+    type: 'MERGE_FILES',
+    files: state.files.files
+  })
+
+  // Open component options
+  store.dispatch({
+    type: 'SHOW_COMPONENT_DETAIL',
+    id: store.getState().components[parent].children[index],
+  })
+
+  store.dispatch({
+    type: 'HIDE_MODAL',
+  })
+}
+
 const ImportTab = ({ parent, index }, { store }) =>
   <Uploader
     accept="application/json"
@@ -11,56 +62,8 @@ const ImportTab = ({ parent, index }, { store }) =>
       fileContents => {
         try {
           // Parse file from JSON
-          const state = fromJSON(fileContents)
-
-          // Hydrate store from resulting object
-          if (state.components.root.children.length === 1) {
-            // If the root component has only a single child,
-            // import that.
-            store.dispatch({
-              type: 'IMPORT_COMPONENT',
-              parent, index,
-              id: state.components.root.children[0],
-              source: state.components,
-            })
-          } else {
-            // If the root component has multiple descendants,
-            // import them as a sequence
-
-            // Remove metadata collection plugin
-            // (which is automatically added to every study root)
-            state.components.root.plugins =
-              state.components.root.plugins.filter(
-                x => x.type !== 'lab.plugins.Metadata'
-              )
-
-            // Rename task from metadata
-            state.components.root.title =
-              state.components.root.metadata.title || 'Unnamed task'
-
-            store.dispatch({
-              type: 'IMPORT_COMPONENT',
-              parent, index,
-              id: 'root',
-              source: state.components,
-            })
-          }
-
-          // Merge files
-          store.dispatch({
-            type: 'MERGE_FILES',
-            files: state.files.files
-          })
-
-          // Open component options
-          store.dispatch({
-            type: 'SHOW_COMPONENT_DETAIL',
-            id: store.getState().components[parent].children[index],
-          })
-
-          store.dispatch({
-            type: 'HIDE_MODAL',
-          })
+          const data = fromJSON(fileContents)
+          importComponent(parent, index, data, store)
         } catch(e) {
           // If things don't work out, let the user know
           alert('Couldn\'t load file, found error', e)
