fde0c0a60f9aaab7973b41047280502bb793bf34
Felix Henninger <mailbox@felixhenninger.com>
2018-11-02 18:56:30 +0100

    Fix variable scoping issue for Open Lab widget

--- packages/builder/src/components/Modal/components/ExportOpenLab/index.js
+++ packages/builder/src/components/Modal/components/ExportOpenLab/index.js
@@ -4,21 +4,25 @@ import Confirm from '../Confirm'
 
 import OpenLabWidget from './widget'
 
-const ExportOpenLab = ({ closeHandler }, { store }) =>
-  <Confirm
+const ExportOpenLab = ({ closeHandler }, { store }) => {
+  // Add variable to keep track of the Open Lab widget
+  let widget
+
+  return <Confirm
     title={ <span>Upload to Open Lab</span> }
     closeLabel="Close"
     closeHandler={ closeHandler }
     confirmLabel="Upload"
     confirmHandler={ () => {
-      this._openLabWidget.submit()
+      widget.submit()
     } }
   >
     <p><strong><a href="https://www.openlab.online/" target="_blank" rel="noopener noreferrer">Open Lab</a></strong> takes care of running your experiment and collecting, storing and managing data. It supports collaboration in projects, customization of task parameters and invitation of participants. The basic version of the application is available for free. </p>
     <OpenLabWidget
-      ref={ widget => this._openLabWidget = widget }
+      ref={ w => widget = w }
     />
   </Confirm>
+}
 
 ExportOpenLab.contextTypes = {
   store: PropTypes.object
