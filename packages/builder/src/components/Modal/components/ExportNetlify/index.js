e2a494d6a3e74bab5e81b0acaea326ae9fc6cb02
Felix Henninger <mailbox@felixhenninger.com>
2018-11-02 18:57:32 +0100

    Fix variable scoping issue for Netlify widget

--- packages/builder/src/components/Modal/components/ExportNetlify/index.js
+++ packages/builder/src/components/Modal/components/ExportNetlify/index.js
@@ -4,23 +4,27 @@ import Confirm from '../Confirm'
 
 import NetlifyWidget from './widget'
 
-const ExportNetlify = ({ closeHandler }, { store }) =>
-  <Confirm
+const ExportNetlify = ({ closeHandler }, { store }) => {
+  // Add variable to keep track of the Open Lab widget
+  let widget
+
+  return <Confirm
     title={ <span>Upload to Netlify</span> }
     closeLabel="Close"
     closeHandler={ closeHandler }
     confirmLabel="Upload"
     confirmHandler={ () => {
-      this._netlifyWidget.triggerSubmit()
+      widget.triggerSubmit()
     } }
   >
     <p><strong><a href="https://www.netlify.com/" target="_blank" rel="noopener noreferrer">Netlify</a> will host your study for you and take care of data collection.</strong> Their service is very easy to use, free for basic studies, and inexpensive for large ones.</p>
     <p>To use Netlify, you'll need to <a href="https://app.netlify.com/signup" target="_blank" rel="noopener noreferrer">set up an account</a> with them. Our documentation describes in more detail <a href="http://labjs.readthedocs.io/en/latest/learn/deploy/4-netlify.html" target="_blank" rel="noopener noreferrer">how to do that</a>.</p>
     <hr />
     <NetlifyWidget
-      ref={ widget => this._netlifyWidget = widget }
+      ref={ w => widget = w }
     />
   </Confirm>
+}
 
 ExportNetlify.contextTypes = {
   store: PropTypes.object
