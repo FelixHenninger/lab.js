55890c05f27268df6601c41e323dd8d802198485
Felix Henninger <mailbox@felixhenninger.com>
2018-12-24 09:10:44 +0100

    Create study header tags dynamically

--- packages/builder/src/logic/io/assemble/html.js
+++ packages/builder/src/logic/io/assemble/html.js
@@ -1,21 +1,35 @@
 import { readDataURI } from '../../util/dataURI'
 import { template } from 'lodash'
 
-const defaultHeader = `
-  <script src="lib/lab.js" data-labjs-script="library"></script>
-  <script src="lib/lab.fallback.js" data-labjs-script="fallback"></script>
-  <link rel="stylesheet" href="lib/lab.css">
-  <!-- study code and styles -->
-  <script defer src="script.js"></script>
-  <link rel="stylesheet" href="style.css">
-`
+// Render HTML from a list of objects
+// (TODO: I'm not sure if this isn't too convoluted.
+// We might switch back to string-based node element
+// generation at some later point)
+const renderNode = ([type, attrs={}]) => {
+  if (type === 'comment') {
+    return `<!-- ${ attrs.content } -->`
+  } else {
+    const node = document.createElement(type)
+    Object.entries(attrs)
+      .forEach(([attr, value]) => node.setAttribute(attr, value))
+    return node.outerHTML
+  }
+}
+
+const defaultHeader = [
+  ['script', { src: 'lib/lab.js', 'data-labjs-script': 'library' }],
+  ['script', { src: 'lib/lab.fallback.js', 'data-labjs-script': 'fallback' }],
+  ['link', { rel: 'stylesheet', href: 'lib/lab.css' }],
+  ['comment', { content: 'study code and styles' }],
+  ['script', { src: 'script.js', defer: true }],
+  ['link', { rel: 'stylesheet', href: 'style.css' }],
+]
 
 const makeHeader = state => {
   // TBC ...
   return defaultHeader
-    .split('\n')
-    .map(l => l.trim())
-    .map((l, i) => i === 0 ? l : `  ${ l }`)
+    .map(renderNode)
+    .map((l, i) => i === 0 ? l : `  ${ l }`) // Indent from second line
     .join('\n')
 }
 
