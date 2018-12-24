9020e9c459953efdba3848168b1fd36001d7aaee
Felix Henninger <mailbox@felixhenninger.com>
2018-12-24 09:24:38 +0100

    Upgrade redux devtools

--- packages/builder/src/store.js
+++ packages/builder/src/store.js
@@ -1,13 +1,15 @@
 import { createStore } from 'redux'
-import reducer from './reducers'
+import { composeWithDevTools } from 'redux-devtools-extension'
 
+import reducer from './reducers'
 import { retrieveState } from './logic/util/persistence'
 import { fromURL } from './logic/util/remoteStudy'
 
 const configureStore = async initialState =>
   createStore(
-    reducer, initialState || await fromURL() || await retrieveState(),
-    window.devToolsExtension && window.devToolsExtension(),
+    reducer,
+    initialState || await fromURL() || await retrieveState(),
+    composeWithDevTools(),
   )
 
 export default configureStore
