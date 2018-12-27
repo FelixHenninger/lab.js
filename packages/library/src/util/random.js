60675bc52f17710d1d8737c97d7748f3cbf7e856
Felix Henninger <mailbox@felixhenninger.com>
2018-12-27 10:08:58 +0100

    Add PRNG seeding function

--- packages/library/src/util/random.js
+++ packages/library/src/util/random.js
@@ -14,6 +14,19 @@ export const uuid4 = (random=Math.random) =>
     v => (v ^ (random() * 16 >> v / 4)).toString(16),
   )
 
+// Seed generation
+// This is adapted from the seedrandom implementation,
+// but (drastically) simplified at the cost of node.js
+// and legacy browser compatibility
+export const autoSeed = (width=256) => {
+  // Create and fill an array of random integers
+  const output = new Uint8Array(width);
+  (window.crypto || window.msCrypto).getRandomValues(output)
+
+  // Output as string of (UTF-16) characters
+  return String.fromCharCode.apply(null, output)
+}
+
 export class Random {
   constructor(options={}) {
     if (options.algorithm === 'alea') {
