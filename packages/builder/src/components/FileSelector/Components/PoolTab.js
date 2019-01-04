de7bd0c15eeb597b42892863a0df84e35fe2db5d
Felix Henninger <mailbox@felixhenninger.com>
2019-01-04 16:11:44 +0100

    Provide access to other components' file pools from PoolTab

--- packages/builder/src/components/FileSelector/Components/PoolTab.js
+++ packages/builder/src/components/FileSelector/Components/PoolTab.js
@@ -1,32 +1,75 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
-import { ListGroup, ListGroupItem } from 'reactstrap'
+import { CustomInput, ListGroup, ListGroupItem } from 'reactstrap'
+import { repeat } from 'lodash'
 
-const PoolTab = ({ component, handleImport }, { store }) => {
-  const components = store.getState().components
-  const files = components[component].files
-    ? components[component].files.rows.map(r => r[0])
-    : []
+import { flatTree } from '../../../logic/tree'
 
-  return <div>
-    <ListGroup>
-      {
-        files.map((f, index) =>
-          <ListGroupItem
-            action tag="a"
-            key={ index }
-            style={{
-              fontFamily: 'Fira Mono'
-            }}
-            onClick={ () => handleImport(component, f.localPath) }
-          >
-            { f.localPath }
-          </ListGroupItem>
-        )
-      }
-    </ListGroup>
-  </div>
+// TODO: Move this component to state hooks
+// once they become available (planned for Q1 2019)
+// (this is a straightforward extension of an
+// earlier version of this component, see git
+// history)
+
+class PoolTab extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      component: this.props.component
+    }
+  }
+
+  render() {
+    const store = this.context.store
+
+    const components = store.getState().components
+    const files = components[this.state.component].files
+      ? components[this.state.component].files.rows.map(r => r[0])
+      : []
+
+    return <div>
+      <CustomInput
+        type="select"
+        id="component"
+        bsSize="lg"
+        className="mb-2 font-weight-bold"
+        defaultValue={ this.state.component }
+        onChange={ e => this.setState({ component: e.target.value }) }
+      >
+        {
+          flatTree(components).map(
+            ([id, level, data], index) =>
+              <option
+                value={ id } key={ index }
+              >
+                { level > 0 ? repeat(' ', level - 1) + '∙ ' : '' }
+                { data.title }
+              </option>
+          )
+        }
+      </CustomInput>
+      <ListGroup>
+        {
+          files.map((f, index) =>
+            <ListGroupItem
+              action tag="a"
+              key={ index }
+              style={{
+                fontFamily: 'Fira Mono'
+              }}
+              onClick={ () => this.props.handleImport(
+                this.state.component,
+                f.localPath
+              ) }
+            >
+              { f.localPath }
+            </ListGroupItem>
+          )
+        }
+      </ListGroup>
+    </div>
+  }
 }
 
 PoolTab.contextTypes = {
