ca609bf5e8caecab363b2805fab4f0c63fdd6427
Felix Henninger <mailbox@felixhenninger.com>
2019-01-09 20:46:22 +0100

    Remove react-loadable in favour of lazy and Suspense

--- packages/builder/src/components/ComponentOptions/components/Content/Canvas/index.js
+++ packages/builder/src/components/ComponentOptions/components/Content/Canvas/index.js
@@ -1,29 +1,28 @@
-import React from 'react'
+import React, { lazy, Suspense } from 'react'
 import PropTypes from 'prop-types'
-import Loadable from 'react-loadable'
 
 import Card from '../../../../Card'
 import { updateComponent } from '../../../../../actions/components'
 
-const LoadableEditor = Loadable({
-  loader: () => import('./editor'),
-  loading() {
-    return <div className="text-center">
-      <i className="fas fa-spinner-third fa-spin my-3" />
-    </div>
-  }
-})
+const CanvasEditor = lazy(() => import('./editor'))
+
+const Spinner = () =>
+  <div className="text-center">
+    <i className="fas fa-spinner-third fa-spin my-3" />
+  </div>
 
 const Editor = ({ id, data }, { store }) =>
   <Card title="Content">
-    <LoadableEditor
-      data={ data.content }
-      onChange={
-        newData => store.dispatch(
-          updateComponent(id, { content: newData })
-        )
-      }
-    />
+    <Suspense fallback={ <Spinner /> }>
+      <CanvasEditor
+        data={ data.content }
+        onChange={
+          newData => store.dispatch(
+            updateComponent(id, { content: newData })
+          )
+        }
+      />
+    </Suspense>
   </Card>
 
 Editor.contextTypes = {
