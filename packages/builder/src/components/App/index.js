54ab791724e26f848101129874452a2b6084779b
Felix Henninger <mailbox@felixhenninger.com>
2019-02-11 08:44:59 +0100

    Enable app-wide keyboard shortcuts

--- packages/builder/src/components/App/index.js
+++ packages/builder/src/components/App/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 
 import Layout from '../Layout'
 import ReduxModal from '../Modal/redux'
+import Shortcuts from '../Shortcuts'
 
 import Sidebar from '../Sidebar'
 import Footer from '../Footer'
@@ -12,6 +13,7 @@ class App extends Component {
   render() {
     return (
       <div>
+        <Shortcuts />
         <ReduxModal />
         <Layout
           sidebar={ <Sidebar /> }
---
+++ packages/builder/src/components/Shortcuts/index.js
@@ -0,0 +1,49 @@
+// Not using JSX, we don't need to import React proper
+import { Component } from 'react'
+import PropTypes from 'prop-types'
+
+import { stateToDownload } from '../../logic/io/save'
+
+// Commands are associated with the command key on macs,
+// and with CTRL elsewhere
+const commandKey = navigator.platform.startsWith('Mac')
+  ? 'metaKey'
+  : 'ctrlKey'
+
+class Shortcuts extends Component {
+  constructor(props) {
+    super(props)
+    this.handleKeyDown = this.handleKeyDown.bind(this)
+  }
+
+  handleKeyDown(e) {
+    if(e[commandKey] && e.key === 's') {
+      e.preventDefault()
+
+      // Save study
+      if (!e.repeat) {
+        stateToDownload(
+          this.context.store.getState(), { removeInternals: e.shiftKey }
+        )
+      }
+    }
+  }
+
+  componentDidMount() {
+    document.addEventListener('keydown', this.handleKeyDown)
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.handleKeyDown)
+  }
+
+  render() {
+    return null
+  }
+}
+
+Shortcuts.contextTypes = {
+  store: PropTypes.object
+}
+
+export default Shortcuts
