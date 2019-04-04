63ecf8281b0baf4f52623d79cf4c4214438f6222
Felix Henninger <mailbox@felixhenninger.com>
2019-04-04 03:54:26 +0200

    Optionally inject synchronisation information in time conversion functions

--- packages/library/src/util/timeline/items/audio.js
+++ packages/library/src/util/timeline/items/audio.js
@@ -42,18 +42,18 @@ const outputTimestamps = (context, useContextTiming=false) => {
   }
 }
 
-const toContextTime = (context, t) => {
+const toContextTime = (context, t, syncTimestamps) => {
   const { contextTime, performanceTime, baseLatency } =
-    outputTimestamps(context)
+    syncTimestamps || outputTimestamps(context)
   return (t - performanceTime) / 1000 + contextTime - baseLatency
 }
 
-const toPerformanceTime = (context, t) => {
+const toPerformanceTime = (context, t, syncTimestamps) => {
   // Where available, use audio system data to calculate
   // latency compensation, as per specification.
   // See https://webaudio.github.io/web-audio-api/
   const { contextTime, performanceTime, baseLatency } =
-    outputTimestamps(context)
+    syncTimestamps || outputTimestamps(context)
   return (t - contextTime + baseLatency) * 1000 + performanceTime
 }
 
