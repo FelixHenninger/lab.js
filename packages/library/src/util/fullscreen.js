67e06d16e723b068ea7c01834c97bea45b53679f
Felix Henninger <mailbox@felixhenninger.com>
2019-05-09 16:39:21 +0200

    Return result of fullscreen request
    The most recent version of the API will return a promise, which will be helpful for checking whether the user accepted the request.

--- packages/library/src/util/fullscreen.js
+++ packages/library/src/util/fullscreen.js
@@ -2,22 +2,22 @@
 
 export const launch = (el) => {
   if (el.requestFullscreen) {
-    el.requestFullscreen()
+    return el.requestFullscreen()
   } else if (el.mozRequestFullScreen) {
-    el.mozRequestFullScreen()
+    return el.mozRequestFullScreen()
   } else if (el.webkitRequestFullscreen) {
-    el.webkitRequestFullscreen()
+    return el.webkitRequestFullscreen()
   } else if (el.msRequestFullscreen) {
-    el.msRequestFullscreen()
+    return el.msRequestFullscreen()
   }
 }
 
 export const exit = () => {
   if (document.exitFullscreen) {
-    document.exitFullscreen()
+    return document.exitFullscreen()
   } else if (document.mozCancelFullScreen) {
-    document.mozCancelFullScreen()
+    return document.mozCancelFullScreen()
   } else if (document.webkitExitFullscreen) {
-    document.webkitExitFullscreen()
+    return document.webkitExitFullscreen()
   }
 }
