122e75293248a46611711ab9326fee1d49474b68
Felix Henninger <mailbox@felixhenninger.com>
2019-06-16 12:57:58 +0200

    Revise page item BaseOptions

--- packages/builder/src/components/ComponentOptions/components/Content/Page/components/ItemOptions/components/BaseOptions.js
+++ packages/builder/src/components/ComponentOptions/components/Content/Page/components/ItemOptions/components/BaseOptions.js
@@ -1,54 +1,66 @@
 import React from 'react'
 import { Control } from 'react-redux-form'
-import { Row, Col, Input, CustomInput } from 'reactstrap'
+import { FormGroup, Input, CustomInput, Label, FormText } from 'reactstrap'
 
 export const BaseOptions = ({ rowIndex }) =>
   <>
     <hr />
-    <Row form className="my-2">
-      <Col>
-        <Control
-          model=".name"
-          placeholder="Name"
-          component={ Input }
-          className="font-weight-bold"
-          controlProps={{
-            style: {
-              fontFamily: 'Fira Mono',
-            }
-          }}
-        />
-      </Col>
-    </Row>
-    <Row form className="my-2">
-      <Col>
-        <Control.checkbox
-          model=".required"
-          component={ CustomInput }
-          controlProps={{
-            type: 'checkbox',
-            label: 'Require answer',
-            id: `item-required-${ rowIndex }`,
-            style: {
-              fontFamily: 'Fira Mono',
-            }
-          }}
-        />
-      </Col>
-    </Row>
-    <Row form className="my-2">
-      <Col>
-        <Control.textarea
-          model=".help"
-          placeholder="Help / further instructions"
-          component={ Input }
-          controlProps={{
-            type: 'textarea', bsSize: 'sm',
-            rows: 3
-          }}
-          className="mt-2 text-muted"
-          style={{ padding: '6px 12px' }}
-        />
-      </Col>
-    </Row>
+    <FormGroup className="my-2">
+      <Label for={ `page-item-${ rowIndex }-help` } className="mb-0">
+        Help &amp; further instructions
+      </Label>
+      <FormText color="muted">
+        Tell your participants more about the question.
+      </FormText>
+      <Control.textarea
+        model=".help"
+        component={ Input }
+        controlProps={{
+          id: `page-item-${ rowIndex }-help`,
+          type: 'textarea', bsSize: 'sm',
+          rows: 3
+        }}
+        className="mt-2 text-muted"
+      />
+    </FormGroup>
+    <hr />
+    <FormGroup className="my-2">
+      <Label for={ `page-item-${ rowIndex }-name` } className="mb-0">
+        Name
+      </Label>
+      <FormText color="muted" className="mb-2">
+        The column in which to save the data
+      </FormText>
+      <Control
+        model=".name"
+        placeholder={ '(auto-generate from question)' }
+        component={ Input }
+        className="font-weight-bold"
+        controlProps={{
+          id: `page-item-${ rowIndex }-name`,
+          style: {
+            fontFamily: 'Fira Mono',
+          }
+        }}
+      />
+    </FormGroup>
+    <hr />
+    <FormGroup className="my-2">
+      Validation
+      <FormText color="muted" className="mb-2">
+        Checks to run before accepting data
+      </FormText>
+      <Control.checkbox
+        model=".required"
+        component={ CustomInput }
+        controlProps={{
+          type: 'checkbox',
+          label: 'Require answer',
+          id: `item-required-${ rowIndex }`,
+          style: {
+            fontFamily: 'Fira Mono',
+          }
+        }}
+      />
+    </FormGroup>
   </>
