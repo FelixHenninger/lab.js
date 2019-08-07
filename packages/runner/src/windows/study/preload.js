e879cb05a7f73b41afc31b857be3336453e33aee
Felix Henninger <mailbox@felixhenninger.com>
2019-08-07 17:56:26 +0200

    Add basic service worker architecture

--- packages/runner/src/windows/study/preload.js
+++ packages/runner/src/windows/study/preload.js
@@ -1,5 +1,43 @@
 const {ipcRenderer} = require('electron')
 
+// Data URI handling -----------------------------------------------------------
+
+const re = /^\s*data:([-+.\w\d]+\/[-+.\w\d]+)?(;base64)?,(.*)$/
+
+const readDataURI = uri => {
+  // Split data URI into constituent parts
+  const [, mime, encoding, data] = re.exec(uri)
+
+  // Is the content encoded as base64?
+  const base64 = encoding === ';base64'
+
+  return {
+    data: base64 ? data : decodeURIComponent(data),
+    mime, base64
+  }
+}
+
+const blobFromDataURI = (uri, atob=window.atob) => {
+  const {data, mime, base64} = readDataURI(uri)
+
+  if (base64) {
+    // Convert base64 to binary
+    const binary = atob(data)
+    // Decode raw bytes
+    const bytes = new Uint8Array(binary.length)
+    for (var i = 0; i < binary.length; i++)        {
+      bytes[i] = binary.charCodeAt(i)
+    }
+    // Return as blob
+    return new Blob([bytes], { type: mime })
+  } else {
+    // Return blob from string data
+    return new Blob([data], { type: mime })
+  }
+}
+
+// -----------------------------------------------------------------------------
+
 console.log('Running preload script in study window')
 
 // Test IPC message passing
@@ -20,4 +58,28 @@ window.addEventListener('keydown', e => {
     console.log('Closing study window')
     window.close()
   }
+})
+
+// Setup study cache -----------------------------------------------------------
+
+caches.open('labjs-preview').then(cache => {
+  cache.put(
+    new Request('https://study.local/index.html'),
+    new Response(blobFromDataURI(
+      'data:text/plain;base64,SGVsbG8gd29ybGQh'
+    ))
+  )
+  console.log('Populated preview cache')
+}).catch(err => {
+  console.log('Error while populating preview cache', err)
+})
+
+// Setup service worker --------------------------------------------------------
+
+navigator.serviceWorker.register('worker.js', {
+  scope: './'
+}).then((sw) => {
+  console.log('Registered preview worker:', sw)
+}).catch((err) => {
+  console.log('Error while registering preview worker', err)
 })
---
+++ packages/runner/src/windows/study/worker.js
@@ -0,0 +1,40 @@
+// Worker initialisation -------------------------------------------------------
+
+const root = self.location.href
+  .split('/')
+  .slice(0, -1) // Remove last path component
+  .join('/')
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    Promise.resolve()
+      .then(() => {
+        console.log(`Preview worker installed at ${ root }`)
+      }).then(() => {
+        // Make the worker the active service worker,
+        // also triggering the activate event
+        self.skipWaiting()
+      }).catch(error => {
+        console.log('Error during preview worker installation', error)
+        throw error
+      })
+  )
+})
+
+// Claim this worker as the active worker for all clients,
+// as per https://serviceworke.rs/immediate-claim.html
+self.addEventListener('activate', function(event) {
+  console.log('Activating preview worker')
+  return self.clients.claim()
+})
+
+// Request handling ------------------------------------------------------------
+
+self.addEventListener('fetch', function(event) {
+  console.log(`Retrieving ${ event.request.url } through preview worker`)
+
+  event.respondWith(
+    caches.open('labjs-preview')
+      .then(cache => cache.match(new Request('https://study.local/index.html')))
+  )
+})
