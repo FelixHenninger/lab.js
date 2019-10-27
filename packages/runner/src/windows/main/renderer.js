d126e9021a7ae9fa274f9cd69cdbaab4f6315bc6
Felix Henninger <mailbox@felixhenninger.com>
2019-10-27 10:33:01 +0100

    Remove esm from main window renderer

--- packages/runner/src/windows/main/renderer.js
+++ packages/runner/src/windows/main/renderer.js
@@ -1,7 +1,8 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
-import {ipcRenderer} from 'electron'
+const {ipcRenderer} = require('electron')
+console.log('got here')
 
 document.body.addEventListener('dragover', e => {
   e.preventDefault()
