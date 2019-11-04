7388ec3fdecec6f1bc54a24daaa284e826344dcf
Felix Henninger <mailbox@felixhenninger.com>
2019-11-04 17:27:10 +0100

    Add (currently non-functional) support for JSON input files

--- utilities/JSON import app/ui.R
+++ utilities/JSON import app/ui.R
@@ -44,45 +44,52 @@ shinyUI(fluidPage(
       'Please select'='',
       'csv (comma delimited)'='csv',
       'csv2 (semicolon delimited)'='csv2',
-      'tsv (tab delimited)'='tsv'
+      'tsv (tab delimited)'='tsv',
+      'JSON'='json'
     )
   ),
   
   # Data column control
-  textInput(
-    "data_column",
-    "Which column contains the lab.js data?",
-    "labjs-data"
+  conditionalPanel(
+    condition="input.data_format !== 'json'", 
+    textInput(
+      "data_column",
+      "Which column contains the lab.js data?",
+      "labjs-data"
+    )
   ),
   
   hr(),
   
   # Data filtering
-  strong('Data filtering'),
-  p(
-    'Some tools (i.e. Qualtrics) include further metadata in the header',
-    'section of their CSV outputs. These are non-standard and cannot',
-    'be usefully loaded into R. If necessary, please exclude them below.'
+  conditionalPanel(
+    condition="input.data_format !== 'json'", 
+    strong('Data filtering'),
+    p(
+      'Some tools (i.e. Qualtrics) include further metadata in the header',
+      'section of their CSV outputs. These are non-standard and cannot',
+      'be usefully loaded into R. If necessary, please exclude them below.'
+    ),
+    p(
+      'We assume that the first row that\'s not skipped is the file header,',
+      'and that the actual data starts only after the skip range.',
+      'Please also note that empty lines are skipped automatically.',
+      class='text-muted'
+    ),
+    checkboxInput(
+      "skip_rows",
+      "Skip header rows in the input file",
+      value=FALSE
+    ),
+    sliderInput(
+      "skip_range",
+      "Rows to skip",
+      min=1, max=10, step=1,
+      value=c(2, 3)
+    ),
+    
+    hr()
   ),
-  p(
-    'We assume that the first row that\'s not skipped is the file header,',
-    'and that the actual data starts only after the skip range.',
-    'Please also note that empty lines are skipped automatically.',
-    class='text-muted'
-  ),
-  checkboxInput(
-    "skip_rows",
-    "Skip header rows in the input file",
-    value=FALSE
-  ),
-  sliderInput(
-    "skip_range",
-    "Rows to skip",
-    min=1, max=10, step=1,
-    value=c(2, 3)
-  ),
-  
-  hr(),
 
   downloadButton(
     "downloadData",
