4e5548c2ddd2e4ec1052b24ce1e32a2226c748de
Raphael Balet <r.balet@ultrastark.ch>
2019-12-04 15:01:53 +0100

    enhance - modal : size

--- packages/builder/src/components/Editor/index.js
+++ packages/builder/src/components/Editor/index.js
@@ -1,14 +1,21 @@
+import { throttle } from 'lodash'
 import React from 'react'
 import MonacoEditor from 'react-monaco-editor'
-
-import { throttle } from 'lodash'
-
 import './editor-style-overrides.css'
 
 export default class Editor extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      editorHeight: 400,
+    }
+  }
+
   editorWillMount(monaco) {
     monaco.editor.defineTheme('labjs', {
-      base: 'vs', inherit: true,
+      base: 'vs',
+      inherit: true,
       rules: [
         { token: 'attribute.name', foreground: '0275d8' },
         { token: 'attribute.value', foreground: '569cd6' },
@@ -46,66 +53,75 @@ export default class Editor extends React.Component {
       // The HTMLHint library loads many dependencies,
       // and is not needed directly when the editor
       // is first loaded, therefore it is split out here.
-      import('htmlhint').then(({ default: HTMLHint }) => {
-        model.onDidChangeContent(throttle(() => {
-          const hints = HTMLHint.verify(
-            model.getValue(), {
-              "tagname-lowercase": true,
-              "attr-lowercase": true,
-              "attr-value-double-quotes": true,
-              "tag-pair": true,
-              "spec-char-escape": true,
-              "id-unique": true,
-              "src-not-empty": true,
-              "attr-no-duplication": true,
-              "attr-unsafe-chars": true
-            }
-          ).map(hint => ({
-            startLineNumber: hint.line, startColumn: hint.col,
-            endLineNumber: hint.line, endColumn: hint.col + hint.raw.length,
-            message: hint.message,
-            severity: 1,
-          }))
+      import('htmlhint')
+        .then(({ default: HTMLHint }) => {
+          model.onDidChangeContent(
+            throttle(() => {
+              const hints = HTMLHint.verify(model.getValue(), {
+                'tagname-lowercase': true,
+                'attr-lowercase': true,
+                'attr-value-double-quotes': true,
+                'tag-pair': true,
+                'spec-char-escape': true,
+                'id-unique': true,
+                'src-not-empty': true,
+                'attr-no-duplication': true,
+                'attr-unsafe-chars': true,
+              }).map(hint => ({
+                startLineNumber: hint.line,
+                startColumn: hint.col,
+                endLineNumber: hint.line,
+                endColumn: hint.col + hint.raw.length,
+                message: hint.message,
+                severity: 1,
+              }))
 
-          monaco.editor.setModelMarkers(
-            model, 'custom-linter', hints
+              this.setEditorHeight(model)
+              monaco.editor.setModelMarkers(model, 'custom-linter', hints)
+            }, 500),
           )
-        }, 500))
-      }).catch(e => console.log('Couldn\'t load HTMLHint:', e))
+        })
+        .catch(e => console.log("Couldn't load HTMLHint:", e))
     }
   }
 
   render() {
-    return <MonacoEditor
-      width="100%"
-      language="html" value={''}
-      theme="labjs"
-      editorDidMount={ this.editorDidMount.bind(this) }
-      editorWillMount={ this.editorWillMount.bind(this) }
-      {...this.props}
-      options={{
-        // Behavior
-        contextmenu: false,
-        cursorBlinking: 'solid',
-        lineNumbers: true,
-        lineNumbersMinChars: 4,
-        overviewRulerLanes: 0,
-        rulers: [80],
-        scrollbar: {
-          useShadows: false,
-        },
-        scrollBeyondLastLine: false,
-        tabSize: 2,
-        // Wrapping
-        wordWrap: 'wordWrapColumn',
-        wordWrapColumn: 80,
-        wrappingIndent: "indent",
-        // Style
-        fontFamily: 'Fira Mono',
-        fontSize: 18,
-        lineHeight: 26,
-        ...this.props.options
-      }}
-    />
+    const editorHeight = this.state.editorHeight // default height
+
+    return (
+      <MonacoEditor
+        language="html"
+        value={''}
+        height="70vh"
+        theme="labjs"
+        editorDidMount={this.editorDidMount.bind(this)}
+        editorWillMount={this.editorWillMount.bind(this)}
+        {...this.props}
+        options={{
+          // Behavior
+          contextmenu: false,
+          cursorBlinking: 'solid',
+          lineNumbers: true,
+          lineNumbersMinChars: 4,
+          overviewRulerLanes: 0,
+          automaticLayout: true,
+          rulers: [80],
+          scrollbar: {
+            useShadows: false,
+          },
+          scrollBeyondLastLine: false,
+          tabSize: 2,
+          // Wrapping
+          wordWrap: 'wordWrapColumn',
+          wordWrapColumn: 80,
+          wrappingIndent: 'indent',
+          // Style
+          fontFamily: 'Fira Mono',
+          fontSize: 18,
+          lineHeight: 26,
+          ...this.props.options,
+        }}
+      />
+    )
   }
 }
--- packages/builder/src/components/Modal/components/Options/index.js
+++ packages/builder/src/components/Modal/components/Options/index.js
@@ -1,55 +1,54 @@
-import React, { useState } from 'react'
 import PropTypes from 'prop-types'
-
-import { ModalBody, ModalFooter, Nav, Button } from 'reactstrap'
-
-import MetadataForm from './MetadataForm'
-import Files from './Files'
-import Editor from '../../../Editor'
-import { NavEntry, NavCloseModal } from '../../../Nav'
-import Icon from '../../../Icon'
-
+import React, { useState } from 'react'
+import { Button, ModalBody, ModalFooter, Nav } from 'reactstrap'
 import { makeDataURI, readDataURI } from '../../../../logic/util/dataURI'
+import Editor from '../../../Editor'
+import Icon from '../../../Icon'
+import { NavCloseModal, NavEntry } from '../../../Nav'
+import Files from './Files'
+import MetadataForm from './MetadataForm'
 import './style.css'
 
 const TabContent = ({ tab }, { store }) => {
   const files = store.getState().files.files
-  switch(tab) {
+  switch (tab) {
     case 'meta':
       return <MetadataForm />
     case 'files':
       return <Files />
     case 'html':
-      return <Editor
-        key="html"
-        height="400"
-        value={ readDataURI(files['index.html'].content).data }
-        onChange={
-          newContent => store.dispatch({
-            type: 'UPDATE_FILE',
-            file: 'index.html',
-            data: {
-              content: makeDataURI(newContent, 'text/html')
-            }
-          })
-        }
-      />
+      return (
+        <Editor
+          key="html"
+          value={readDataURI(files['index.html'].content).data}
+          onChange={newContent =>
+            store.dispatch({
+              type: 'UPDATE_FILE',
+              file: 'index.html',
+              data: {
+                content: makeDataURI(newContent, 'text/html'),
+              },
+            })
+          }
+        />
+      )
     case 'css':
-      return <Editor
-        key="css"
-        height="400"
-        language="css"
-        value={ readDataURI(files['style.css'].content).data }
-        onChange={
-          newContent => store.dispatch({
-            type: 'UPDATE_FILE',
-            file: 'style.css',
-            data: {
-              content: makeDataURI(newContent, 'text/css')
-            }
-          })
-        }
-      />
+      return (
+        <Editor
+          key="css"
+          language="css"
+          value={readDataURI(files['style.css'].content).data}
+          onChange={newContent =>
+            store.dispatch({
+              type: 'UPDATE_FILE',
+              file: 'style.css',
+              data: {
+                content: makeDataURI(newContent, 'text/css'),
+              },
+            })
+          }
+        />
+      )
     default:
       return <div>Requested tab not found</div>
   }
@@ -65,28 +64,25 @@ const OptionsModal = ({ closeHandler }) => {
   return (
     <div className="modal-content">
       <Nav tabs>
-        <NavEntry id="meta" activeId={ tab } setId={ setTab }>
+        <NavEntry id="meta" activeId={tab} setId={setTab}>
           <Icon icon="info" weight="s" />
         </NavEntry>
-        <NavEntry id="files" activeId={ tab } setId={ setTab }>
+        <NavEntry id="files" activeId={tab} setId={setTab}>
           <Icon icon="folder" />
         </NavEntry>
-        <NavEntry id="html" activeId={ tab } setId={ setTab }>
+        <NavEntry id="html" activeId={tab} setId={setTab}>
           HTML
         </NavEntry>
-        <NavEntry id="css" activeId={ tab } setId={ setTab }>
+        <NavEntry id="css" activeId={tab} setId={setTab}>
           CSS
         </NavEntry>
-        <NavCloseModal onClick={ closeHandler } />
+        <NavCloseModal onClick={closeHandler} />
       </Nav>
       <ModalBody>
-        <TabContent tab={ tab } />
+        <TabContent tab={tab} />
       </ModalBody>
       <ModalFooter>
-        <Button
-          outline color="secondary"
-          onClick={ closeHandler }
-        >
+        <Button outline color="secondary" onClick={closeHandler}>
           Done
         </Button>
       </ModalFooter>
