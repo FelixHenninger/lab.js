3c7e6bb4026771912c4cf06bb1f99d2c0e3639b7
Felix Henninger <mailbox@felixhenninger.com>
2020-01-02 17:26:48 +0100

    Add builder integration for the fullscreen plugin

--- packages/builder/src/logic/plugins/library.js
+++ packages/builder/src/logic/plugins/library.js
@@ -1,3 +1,24 @@
+const fullscreen = {
+  title: 'Fullscreen',
+  description: 'Enable fullscreen mode for portions of the study',
+  version: '0.1.0',
+  path: 'lab.plugins.Fullscreen',
+  files: {},
+  headers: [],
+  options: {
+    'message': {
+      label: 'Message',
+      type: 'text',
+      placeholder: 'This experiment requires full screen display',
+    },
+    'hint': {
+      label: 'Help text',
+      type: 'text',
+      placeholder: 'Please click to continue in full screen mode',
+    }
+  }
+}
+
 const mousetrap = {
   title: 'Mousetrap',
   description: 'Mouse-tracking data collection',
@@ -25,6 +46,7 @@ const mousetrap = {
 }
 
 export const plugins = {
+  fullscreen,
   mousetrap,
 }
 
