104c31fcd3ce174ad2fdc170364a473a6cde285a
Felix Henninger <mailbox@felixhenninger.com>
2020-01-26 10:06:39 +0100

    Refactor TemplateTab to use useStore hook

--- packages/builder/src/components/Modal/components/AddComponent/components/templateTab/index.js
+++ packages/builder/src/components/Modal/components/AddComponent/components/templateTab/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import PropTypes from 'prop-types'
+import { useStore } from 'react-redux'
 import { Card } from 'reactstrap'
 import { TaskList } from '../../../../../ComponentOptions/components/Welcome/components/ExampleTab'
 
@@ -9,35 +9,36 @@ const basePath =
   process.env.REACT_APP_TEMPLATE_PATH ??
   'https://raw.githubusercontent.com/FelixHenninger/lab.js/master/templates/'
 
-const TemplateTab = ({ parent, index }, { store }) =>
-  <div>
-    <h2 className="h5 mt-1">
-      <span>Templates</span>
-    </h2>
-    <p>Click on any template to include it in your study; they're yours to use and extend. Please consider <a href="https://labjs.readthedocs.io/en/latest/meta/contribute/index.html">sharing your own favorites!</a></p>
-    <Card className="card-flush">
-      <TaskList
-        path={ basePath }
-        taskLabel="templates"
-        loadHandler={ data => {
-          try {
-            importComponent(parent, index, data, store)
-          } catch(e) {
-            // If things don't work out, let the user know
-            alert('Couldn\'t load file, found error', e)
-          }
-        } }
-      />
-    </Card>
-    <div className="mt-3">
-      <small className="text-muted">
-        Missing something? Ideas for improving things? Please <a href="https://labjs.readthedocs.io/en/latest/meta/contribute/index.html" target="_blank" rel="noopener noreferrer">suggest or contribute</a>!
-      </small>
-    </div>
-  </div>
+const TemplateTab = ({ parent, index }) => {
+  const store = useStore()
 
-TemplateTab.contextTypes = {
-  store: PropTypes.object,
+  return (
+    <div>
+      <h2 className="h5 mt-1">
+        <span>Templates</span>
+      </h2>
+      <p>Click on any template to include it in your study; they're yours to use and extend. Please consider <a href="https://labjs.readthedocs.io/en/latest/meta/contribute/index.html">sharing your own favorites!</a></p>
+      <Card className="card-flush">
+        <TaskList
+          path={ basePath }
+          taskLabel="templates"
+          loadHandler={ data => {
+            try {
+              importComponent(parent, index, data, store)
+            } catch(e) {
+              // If things don't work out, let the user know
+              alert('Couldn\'t load file, found error', e)
+            }
+          } }
+        />
+      </Card>
+      <div className="mt-3">
+        <small className="text-muted">
+          Missing something? Ideas for improving things? Please <a href="https://labjs.readthedocs.io/en/latest/meta/contribute/index.html" target="_blank" rel="noopener noreferrer">suggest or contribute</a>!
+        </small>
+      </div>
+    </div>
+  )
 }
 
 export default TemplateTab
