a7bef5ca3dbf859d26afb056d647fef4357b53df
Felix Henninger <mailbox@felixhenninger.com>
2020-01-27 03:20:34 +0100

    Remove migration from LocalStorage to LocalForage

--- packages/builder/src/logic/util/persistence.js
+++ packages/builder/src/logic/util/persistence.js
@@ -1,5 +1,5 @@
 import localForage from 'localforage'
-import { fromJSON, fromObject } from '../io/load'
+import { fromObject } from '../io/load'
 
 const lf = localForage.createInstance({
   name: "lab.js"
@@ -29,17 +29,6 @@ export const persistState = async store => {
 }
 
 export const retrieveState = async () => {
-  // Migrate storage from localStorage to localForage/IDB
-  // TODO: Remove migration eventually (2019 release)
-  const localStorage = window.localStorage
-  if (localStorage && localStorage.getItem('labjs:state:default')) {
-    await lf.setItem(
-      'state:latest',
-      fromJSON(localStorage.getItem('labjs:state:default')),
-    )
-    localStorage.clear()
-  }
-
   // Load data from localForage
   const data = await lf.getItem('state:latest')
 
