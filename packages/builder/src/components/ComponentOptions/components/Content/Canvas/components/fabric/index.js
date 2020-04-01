55351686a9c0dd17fdf0cdeec8829f28b377ac39
Felix Henninger <mailbox@felixhenninger.com>
2020-04-01 13:18:02 +0200

    Default to sans-serif font in the graphical stimulus designer

--- packages/builder/src/components/ComponentOptions/components/Content/Canvas/components/fabric/index.js
+++ packages/builder/src/components/ComponentOptions/components/Content/Canvas/components/fabric/index.js
@@ -346,7 +346,7 @@ export default class FabricCanvas extends Component {
           const content = options.content
           delete options.content
           return new fabric.IText(content, {
-            fontFamily: 'serif',
+            fontFamily: 'sans-serif',
             fontSize: 32,
             textAlign: 'center',
             ...defaults,
