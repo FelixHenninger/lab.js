141bc1f11c76feb841cd0d621b5ed0cf16484cb7
Felix Henninger <mailbox@felixhenninger.com>
2020-07-29 11:18:07 +0200

    Pass additional props to formik through custom wrapper

--- packages/builder/src/components/ComponentOptions/components/Form.js
+++ packages/builder/src/components/ComponentOptions/components/Form.js
@@ -12,10 +12,12 @@ const Form = ({
   updateComponent,
   postProcess=d => d,
   children,
+  ...props
 }) =>
   <Formik
     initialValues={ pick(data, keys) }
     enableReinitialize={ true }
+    { ...props }
   >
     <>
       { children }
