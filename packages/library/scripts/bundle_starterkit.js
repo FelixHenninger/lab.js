d1383da4b2641c240d957a0aafaac5fbed4bde60
Dano Morrison <morrisondano@gmail.com>
2020-09-01 16:59:26 -0700

    Renamed script to js

--- packages/library/scripts/bundle_starterkit.js
+++ packages/library/scripts/bundle_starterkit.js
@@ -1,11 +1,13 @@
-// @ts-expect-error ts-migrate(2451) FIXME: Cannot redeclare block-scoped variable 'shell'.
-const shell = require('shelljs')
 
 // Copy files ------------------------------------------------------------------
 shell.mkdir('-p', 'dist/labjs-starterkit/lib')
 shell.cp('-R', 'src/starterkit/index.html', 'dist/labjs-starterkit')
 shell.cp('-R', 'src/starterkit/study.js', 'dist/labjs-starterkit')
-shell.cp('-R', 'src/starterkit/lib/lab.fallback.js', 'dist/labjs-starterkit/lib')
+shell.cp(
+  '-R',
+  'src/starterkit/lib/lab.fallback.js',
+  'dist/labjs-starterkit/lib',
+)
 shell.cp('-R', 'dist/lab.dev.js', 'dist/labjs-starterkit/lib')
 shell.cp('-R', 'dist/lab.js', 'dist/labjs-starterkit/lib')
 shell.cp('-R', 'dist/lab.js.map', 'dist/labjs-starterkit/lib')
@@ -18,16 +20,16 @@ shell.cp('-R', 'src/starterkit/lib/loading.svg', 'dist/labjs-starterkit/lib')
 // (TODO: Think about doing this directly from the source files)
 const JSZip = require('jszip')
 const fs = require('fs')
-// @ts-expect-error ts-migrate(2304) FIXME: Cannot find name 'zip'.
 zip = new JSZip()
 
-shell.ls('-R', 'dist/labjs-starterkit/**/*')
+shell
+  .ls('-R', 'dist/labjs-starterkit/**/*')
   .filter((filename: any) => filename.startsWith('dist/labjs-starterkit'))
   .forEach((filename: any) => {
     // @ts-expect-error ts-migrate(2304) FIXME: Cannot find name 'zip'.
     zip.file(
       filename.replace(/^dist\/labjs-starterkit\//, ''),
-      fs.readFileSync(filename)
+      fs.readFileSync(filename),
     )
   })
 
@@ -35,8 +37,10 @@ shell.ls('-R', 'dist/labjs-starterkit/**/*')
 // @ts-expect-error ts-migrate(2304) FIXME: Cannot find name 'zip'.
 zip
   .generateNodeStream({
-    compression: 'DEFLATE', compressionOptions: { level: 9 },
-    type:'nodebuffer', streamFiles:true,
+    compression: 'DEFLATE',
+    compressionOptions: { level: 9 },
+    type: 'nodebuffer',
+    streamFiles: true,
   })
   .pipe(fs.createWriteStream('dist/labjs-starterkit.zip'))
   .on('finish', () => console.log('Created starterkit bundle'))
