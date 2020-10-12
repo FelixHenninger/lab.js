b5b47ee26f19c6d55e13ed11ca63f0d80cf66a32
Dano Morrison <morrisondano@gmail.com>
2020-10-12 14:22:34 -0400

    Updated exports to work in outside typescript projects better

--- packages/library/src/index.ts
+++ packages/library/src/index.ts
@@ -1,38 +1,6 @@
 // Load standard library polyfill
 import 'core-js/stable'
 
-// Components
-import { Component, Dummy } from './core'
-import { Screen as CanvasScreen, Frame as CanvasFrame } from './canvas'
-import { Screen, Form, Frame, Page } from './html'
-import { Sequence, Parallel, Loop } from './flow'
-
-// Data storage
-import { Store } from './data'
-
-// Utilities
-import { Random } from './util/random'
-import fromObject from './util/fromObject'
-import { transform, makeRenderFunction } from './util/canvas'
-import { product } from './util/combinatorics'
-import { logTimestamp } from './util/events'
-import { distance, toRadians, polygon, polygonVertex } from './util/geometry'
-import { launch, exit } from './util/fullscreen'
-import { sum, mean, variance, std } from './util/stats'
-import { FrameTimeout } from './util/timing'
-import { traverse, reduce } from './util/tree'
-
-// Plugins
-import Debug from './plugins/debug'
-import Download from './plugins/download'
-import Fullscreen from './plugins/fullscreen'
-import Logger from './plugins/log'
-import Metadata from './plugins/metadata'
-import NavigationGuard from './plugins/navigationGuard'
-import PostMessage from './plugins/postmessage'
-import Submit from './plugins/submit'
-import Transmit from './plugins/transmit'
-
 export const version = '20.1.1'
 export const build = {
   // @ts-expect-error ts-migrate(2304) FIXME: Cannot find name 'BUILD_FLAVOR'.
@@ -41,79 +9,14 @@ export const build = {
   commit: BUILD_COMMIT,
 }
 
-export const core = {
-  Component,
-  Dummy,
-}
+export * as core from './core'
 
-export const canvas = {
-  Frame: CanvasFrame,
-  Screen: CanvasScreen,
-}
+export * as canvas from './canvas'
 
-export const html = {
-  Screen,
-  Form,
-  Frame,
-  Page,
-}
+export * as html from './html'
 
-export const flow = {
-  Sequence,
-  Parallel,
-  Loop,
-}
+export * as flow from './flow'
 
-export const plugins = {
-  Debug,
-  Download,
-  Fullscreen,
-  Logger,
-  Metadata,
-  NavigationGuard,
-  PostMessage,
-  Submit,
-  Transmit,
-}
+export * as data from './data'
 
-export const data = {
-  Store,
-}
-
-export const util = {
-  Random,
-  fromObject,
-  canvas: {
-    makeRenderFunction,
-    transform,
-  },
-  combinatorics: {
-    product,
-  },
-  events: {
-    logTimestamp,
-  },
-  fullscreen: {
-    launch,
-    exit,
-  },
-  geometry: {
-    distance,
-    polygon,
-    polygonVertex,
-    toRadians,
-  },
-  stats: {
-    sum,
-    mean,
-    variance,
-    std,
-  },
-  timing: {
-    FrameTimeout,
-  },
-  tree: {
-    traverse,
-    reduce,
-  },
-}
+export * as util from './util'
---
+++ packages/library/src/util/index.ts
@@ -0,0 +1,13 @@
+import fromObject from './fromObject'
+
+// Utilities
+export { Random } from './random'
+export { default as fromObject } from './fromObject'
+export { transform, makeRenderFunction } from './canvas'
+export { product } from './combinatorics'
+export { logTimestamp } from './events'
+export { distance, toRadians, polygon, polygonVertex } from './geometry'
+export { launch, exit } from './fullscreen'
+export { sum, mean, variance, std } from './stats'
+export { FrameTimeout } from './timing'
+export { traverse, reduce } from './tree'
