e803deb0445a98e7ea2aeb5585c99f794a23ae4b
Felix Henninger <mailbox@felixhenninger.com>
2020-12-02 18:22:46 +0100

    Remove duplicate check from deployment script
    The logic is already covered by the CI configuration

--- packages/builder/scripts/deploy.js
+++ packages/builder/scripts/deploy.js
@@ -1,17 +1,9 @@
 const shell = require('shelljs')
-const { CI, TRAVIS_PULL_REQUEST, TRAVIS_BRANCH } = process.env
 
-if (!(
-  CI === 'true' && (
-    TRAVIS_BRANCH !== 'master' ||
-    TRAVIS_PULL_REQUEST === 'true'
-  )
-)) {
-  shell.exec('cd build && zip -r ../dist.zip .')
-  shell.exec('curl ' +
-    '-H "Content-Type: application/zip" ' +
-    '-H "Authorization: Bearer $NETLIFY_ACCESS_TOKEN" ' +
-    '--data-binary "@dist.zip" ' +
-    'https://api.netlify.com/api/v1/sites/$NETLIFY_SITE_ID/deploys'
-  )
-}
+shell.exec('cd build && zip -r ../dist.zip .')
+shell.exec('curl ' +
+  '-H "Content-Type: application/zip" ' +
+  '-H "Authorization: Bearer $NETLIFY_ACCESS_TOKEN" ' +
+  '--data-binary "@dist.zip" ' +
+  'https://api.netlify.com/api/v1/sites/$NETLIFY_SITE_ID/deploys'
+)
