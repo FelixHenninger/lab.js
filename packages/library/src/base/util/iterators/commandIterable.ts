813038755d048349ab46ac79cf046439f40070f2
Felix Henninger <felix.henninger@web.de>
2021-10-24 17:13:04 +0200

    Refactor CommandIterable to add target stack transitions

--- packages/library/src/base/util/iterators/commandIterable.ts
+++ packages/library/src/base/util/iterators/commandIterable.ts
@@ -1,11 +1,16 @@
+import { isEqual } from 'lodash'
 import { Component } from '../../component'
+
+const isLeaf = (node: Component) => node.internals.iterator === undefined
 
 export class CommandIterable {
   stack: Component[]
+  targetStack: Component[] | undefined
   abortLevel?: number
 
   constructor(root: Component) {
     this.stack = [root]
+    this.targetStack = undefined
   }
 
   abort(component: Component) {
@@ -13,7 +18,7 @@ export class CommandIterable {
     if (level === -1) {
       throw new Error('Aborting from component not in current stack')
     } else {
-      this.abortLevel = level
+      this.targetStack = this.stack.slice(0, level)
     }
   }
 
@@ -22,52 +27,63 @@ export class CommandIterable {
       next: (): IteratorResult<[string, Component, boolean]> => {
         const currentLevel = this.stack.length - 1
         const currentNode = this.stack[currentLevel]
-        const leaf = currentNode?.internals.iterator === undefined
 
         // Special cases:
         if (this.stack.length === 0) {
-          // Root component done
+          // Root component done: Stack clear
           return {
             done: true,
-            value: ['done', currentNode, leaf],
+            value: ['done', undefined, true],
           }
-        } else if (
-          this.abortLevel !== undefined &&
-          currentLevel >= this.abortLevel
-        ) {
-          // Component abort
-          this.stack.pop()
-          return {
-            done: false,
-            value: ['end', currentNode, leaf],
-          }
-        } else if (this.abortLevel && currentLevel < this.abortLevel) {
-          // Abort done
-          this.abortLevel = undefined
         } else if (currentNode.status < 2) {
           // Root component not started
           return {
             done: false,
-            value: ['run', currentNode, leaf],
+            value: ['run', currentNode, isLeaf(currentNode)],
+          }
+        } else if (this.targetStack) {
+          // Transition to target stack
+          if (isEqual(this.stack, this.targetStack)) {
+            // If transition is complete, clear targetStack and move on
+            this.targetStack = undefined
+          } else if (
+            isEqual(this.stack, this.targetStack.slice(0, this.stack.length))
+          ) {
+            // If the transition is partially complete,
+            // append the next stack level
+            const nested = this.targetStack[currentLevel + 1]
+            this.stack.push(nested)
+            return {
+              done: false,
+              value: ['run', nested, isLeaf(nested)],
+            }
+          } else {
+            // Otherwise, move up a level
+            this.stack.pop()
+            return {
+              done: false,
+              value: ['end', currentNode, isLeaf(currentNode)],
+            }
           }
         }
 
+        // Note that this assumes an array-like iterator
+        // that returns [index, value] pairs
         const nested = currentNode.internals.iterator?.next().value?.[1]
 
         if (nested) {
           // Move down
-          const nestedLeaf = nested?.internals.iterator === undefined
           this.stack.push(nested)
           return {
             done: false,
-            value: ['run', nested, nestedLeaf],
+            value: ['run', nested, isLeaf(nested)],
           }
         } else {
           // Move up
           this.stack.pop()
           return {
             done: false,
-            value: ['end', currentNode, leaf],
+            value: ['end', currentNode, isLeaf(currentNode)],
           }
         }
       },
