1a19761dd0bcf1049bfaa9e819015d99c8a1b90c
Felix Henninger <felix.henninger@web.de>
2021-10-31 17:48:10 +0100

    Test stack jumping

--- packages/library/src/base/util/iterators/commandIterable.test.ts
+++ packages/library/src/base/util/iterators/commandIterable.test.ts
@@ -64,3 +64,39 @@ test('Can abort a leaf component', () => {
   expect(iterator.next().value).toEqual(['end', a1, false])
   expect(iterator.next().value).toEqual(['run', a2, true])
 })
+
+test('Can jump stacks', () => {
+  const { a1, a2, root } = makeStubTree()
+
+  // Nested tree beneath a1
+  const b1 = makeStubComponent('b1')
+  const b2 = makeStubComponent('b2')
+  a1.internals = {
+    iterator: [b1, b2].entries(),
+  }
+
+  const c1 = makeStubComponent('c1')
+  const c2 = makeStubComponent('c2')
+  a2.internals = {
+    iterator: [c1, c2].entries(),
+  }
+
+  //@ts-ignore We're faking components here
+  const tree = new CommandIterable(root)
+  const iterator = tree[Symbol.iterator]()
+
+  // Skipping ['run', root, false], see above
+  expect(iterator.next().value).toEqual(['run', a1, false])
+  expect(iterator.next().value).toEqual(['run', b1, true])
+  //@ts-ignore Again, working with shims here
+  tree.targetStack = [root, a2, c2]
+  expect(iterator.next().value).toEqual(['end', b1, true])
+  // Important: Skipping b2 here
+  expect(iterator.next().value).toEqual(['end', a1, false])
+  expect(iterator.next().value).toEqual(['run', a2, false])
+  // Important: Skipping c1 here
+  expect(iterator.next().value).toEqual(['run', c2, true])
+  expect(iterator.next().value).toEqual(['end', c2, true])
+  // Note that this doesn't test that the a2 iterator is updated,
+  // so the next drawn component drawn would be wrong
+})
