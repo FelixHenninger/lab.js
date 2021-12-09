4ed5f7f6dd8ec0b69e3e87aef55b876e36fdfca1
Felix Henninger <felix.henninger@web.de>
2021-12-09 18:10:28 +0100

    Implement findSplice and fastForward

--- packages/library/src/base/util/iterators/timeline.test.ts
+++ packages/library/src/base/util/iterators/timeline.test.ts
@@ -20,8 +20,7 @@ const makeExample = () => {
 it('works with array tree', () => {
   const { a1, b1, b2, c1, c2, c3 } = makeExample()
 
-  //@ts-ignore
-  const s = new SliceIterator<TestObject>(a1)
+  const s = new SliceIterator(a1)
   const output = Array.from(s)
 
   expect(output[0]).toEqual([a1, b1, c1])
@@ -29,15 +28,56 @@ it('works with array tree', () => {
   expect(output[2]).toEqual([a1, b2, c3])
 })
 
-it('can splice a tree', () => {
-  const { a1, b1, b2, c1, c2, c3 } = makeExample()
-  //@ts-ignore
-  const s = new SliceIterator<TestObject>(a1)
+it('can splice a tree based on level index', () => {
+  const { a1, b1, b2, c1, c3 } = makeExample()
+
+  const s = new SliceIterator(a1)
 
   const iterator = s[Symbol.iterator]()
 
   expect(iterator.next()?.value).toEqual([a1, b1, c1])
-  //@ts-ignore
   iterator.splice(1)
   expect(iterator.next()?.value).toEqual([a1, b2, c3])
 })
+
+it('can splice a tree based on an intermediate object', () => {
+  const { a1, b1, b2, c1, c3 } = makeExample()
+
+  const s = new SliceIterator(a1)
+
+  const iterator = s[Symbol.iterator]()
+  expect(iterator.next()?.value).toEqual([a1, b1, c1])
+  iterator.findSplice(b1)
+  expect(iterator.next()?.value).toEqual([a1, b2, c3])
+})
+
+it('ends iteration after top-level splice', () => {
+  const { a1, b1, c1 } = makeExample()
+
+  const s = new SliceIterator<TestObject>(a1)
+
+  const iterator = s[Symbol.iterator]()
+  expect(iterator.next()?.value).toEqual([a1, b1, c1])
+  iterator.findSplice(a1)
+  expect(iterator.next()?.done).toEqual(true)
+})
+
+it('can fast-forward', () => {
+  const { a1, b1, b2, c1, c3 } = makeExample()
+
+  const s = new SliceIterator<TestObject>(a1)
+
+  const iterator = s[Symbol.iterator]()
+  expect(iterator.next()?.value).toEqual([a1, b1, c1])
+
+  iterator.fastForward((v, level) => {
+    if (level === 1) {
+      return v === b2
+    } else {
+      return v === c3
+    }
+  })
+  expect(iterator.next().value).toEqual([a1, b2, c3])
+})
+
+// What would be good tests?
--- packages/library/src/base/util/iterators/timeline.ts
+++ packages/library/src/base/util/iterators/timeline.ts
@@ -1,16 +1,37 @@
-export class SliceIterator<T> {
-  #root: Iterable<T | Iterable<T>>
+import { fastForward } from './fastforward'
 
-  constructor(root: Iterable<T>) {
+interface NestedIterable<T> extends Iterable<T | NestedIterable<T>> {}
+interface TimelineIterator<T> extends Iterator<(T | NestedIterable<T>)[]> {
+  splice: (level: number) => void
+  findSplice: (value: T | NestedIterable<T>) => void
+  fastForward: (
+    consume: (value: T | NestedIterable<T>, level: number) => boolean,
+  ) => void
+}
+
+export class SliceIterator<T> {
+  #root: NestedIterable<T>
+
+  constructor(root: NestedIterable<T>) {
     this.#root = root
   }
 
-  [Symbol.iterator](): Iterator<Array<T | Iterable<T>>> {
+  [Symbol.iterator](): TimelineIterator<T> {
     const iteratorStack = [this.#root[Symbol.iterator]()]
-    const outputStack = [this.#root]
+    const outputStack: (T | NestedIterable<T>)[] = [this.#root]
+    let tempLeaf: T | undefined = undefined
 
     return {
-      next: (): IteratorResult<T> => {
+      next: (): IteratorResult<(T | NestedIterable<T>)[]> => {
+        // If another method has created a temporary stack,
+        // output that, and then make sure we return to
+        // normal iteration on the next call.
+        if (tempLeaf) {
+          const output = [...outputStack, tempLeaf]
+          tempLeaf = undefined
+          return { value: output, done: false }
+        }
+
         while (iteratorStack.length > 0) {
           const currentLeaf = iteratorStack[iteratorStack.length - 1]
           const { value, done } = currentLeaf.next()
@@ -30,12 +51,53 @@ export class SliceIterator<T> {
 
         return { value: undefined, done: true }
       },
-      //@ts-ignore
       splice: level => {
         iteratorStack.splice(level)
         outputStack.splice(level)
       },
-      }
+      findSplice: function (value: T | NestedIterable<T>) {
+        const level = outputStack.indexOf(value)
+        if (level >= 0) {
+          this.splice(level)
+        }
+      },
+      fastForward: (
+        consume: (value: T | NestedIterable<T>, level: number) => boolean,
+      ) => {
+        // Starting from the root node ...
+        iteratorStack.splice(1)
+        outputStack.splice(1)
+        let currentLevel = 0
+
+        // ... rebuild the iterator stack and
+        // the output level by level
+        while (true) {
+          const iterator = iteratorStack[currentLevel]
+          const [{ value, done }] = fastForward(iterator, v =>
+            consume(v, currentLevel + 1),
+          )
+
+          // If we reach an iterator that's done,
+          // we stop there and let the regular algorithm
+          // take over from there (continuing one level up)
+          if (done) {
+            break
+          }
+
+          // Continue if the next entry
+          // is also an iterator
+          if (Symbol.iterator in value) {
+            iteratorStack.push(value[Symbol.iterator]())
+            outputStack.push(value)
+            currentLevel += 1
+          } else {
+            // If we run into a leaf node, that's
+            // where we pick back up.
+            tempLeaf = value
+            break
+          }
+        }
+      },
     }
   }
 }
