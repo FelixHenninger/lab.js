c8b71523b7b045bc8f82f757c7377990c5be9d77
Felix Henninger <felix.henninger@web.de>
2022-03-05 11:47:55 +0100

    Add event types to plugin API

--- packages/library/src/base/plugin.ts
+++ packages/library/src/base/plugin.ts
@@ -1,15 +1,21 @@
 import { without } from 'lodash'
 import { Component } from './component'
 
-export type Plugin = {
-  handle: (context: Component, event: string, data?: any) => Promise<void>
+type PluginEvent = 'plugin:init' | 'plugin:removal'
+
+export type Plugin<T = string> = {
+  handle: (
+    context: Component,
+    event: T | PluginEvent,
+    data?: any,
+  ) => Promise<void>
 }
 
-export class PluginAPI {
-  plugins: Array<Plugin>
+export class PluginAPI<T = string> {
+  plugins: Array<Plugin<T>>
   context: Component
 
-  constructor(context: Component, plugins: Array<Plugin> = []) {
+  constructor(context: Component, plugins: Array<Plugin<T>> = []) {
     this.context = context
     this.plugins = plugins
 
@@ -21,17 +27,17 @@ export class PluginAPI {
     this.context.internals.emitter.on('*', this.handle)
   }
 
-  add(plugin: Plugin) {
+  add(plugin: Plugin<T>) {
     this.plugins.push(plugin)
     plugin.handle(this.context, 'plugin:init')
   }
 
-  remove(plugin: Plugin) {
+  remove(plugin: Plugin<T>) {
     plugin.handle(this.context, 'plugin:removal')
     this.plugins = without(this.plugins, plugin)
   }
 
-  async handle(event: string, data: any) {
+  async handle(event: T, data: any) {
     await Promise.all(
       this.plugins.map(p => p.handle(this.context, event, data)),
     )
