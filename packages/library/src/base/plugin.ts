128bfccbc7a4ce8e634299d6bf9c1eaf88edeb2e
Felix Henninger <felix.henninger@web.de>
2022-08-19 16:07:38 +0200

    Automatically call eponymous methods on plugin events

--- packages/library/src/base/plugin.ts
+++ packages/library/src/base/plugin.ts
@@ -1,13 +1,23 @@
 import { without } from 'lodash'
 import { Component } from './component'
+import { getEventMethodName } from './util/eventName'
 
 type PluginEvent = 'plugin:add' | 'plugin:remove'
 
-export class Plugin<C extends Component = Component, E = string> {
-  async handle(context: C, event: E | PluginEvent, data?: any) {}
+export class Plugin<
+  C extends Component = Component,
+  E extends string = string,
+> {
+  async handle(context: C, event: E | PluginEvent, ...params: any[]) {
+    //@ts-ignore Dynamic dispatch is too much for TS
+    this[getEventMethodName(event)]?.call(this, context, ...params)
+  }
 }
 
-export class PluginAPI<C extends Component = Component, E = string> {
+export class PluginAPI<
+  C extends Component = Component,
+  E extends string = string,
+> {
   plugins: Array<Plugin<C, E>>
   context: C
 
@@ -33,9 +43,9 @@ export class PluginAPI<C extends Component = Component, E = string> {
     this.plugins = without(this.plugins, plugin)
   }
 
-  async handle(event: E, data: any) {
+  async handle(event: E, ...data: any[]) {
     await Promise.all(
-      this.plugins.map(p => p.handle(this.context, event, data)),
+      this.plugins.map(p => p.handle(this.context, event, ...data)),
     )
   }
 }
