f1d0f87d4703de1093460d3002f24bb870b93f5d
Felix Henninger <mailbox@felixhenninger.com>
2023-02-08 15:58:49 +0100

    Make context on plugin API private

--- packages/library/src/base/plugin.ts
+++ packages/library/src/base/plugin.ts
@@ -9,33 +9,33 @@ export class Plugin<C extends Component = Component, E = string> {
 
 export class PluginAPI<C extends Component = Component, E = string> {
   plugins: Array<Plugin<C, E>>
-  context: C
+  #context: C
 
   constructor(context: C, plugins: Array<Plugin<C, E>> = []) {
-    this.context = context
+    this.#context = context
     this.plugins = plugins
 
     // Initialize existing plugins
-    this.plugins.forEach(p => p.handle(this.context, 'plugin:add'))
+    this.plugins.forEach(p => p.handle(this.#context, 'plugin:add'))
 
     // Setup event handlers
     this.handle = this.handle.bind(this)
-    this.context.internals.emitter.on('*', this.handle)
+    this.#context.internals.emitter.on('*', this.handle)
   }
 
   add(plugin: Plugin<C, E>) {
     this.plugins.push(plugin)
-    plugin.handle(this.context, 'plugin:add')
+    plugin.handle(this.#context, 'plugin:add')
   }
 
   remove(plugin: Plugin<C, E>) {
-    plugin.handle(this.context, 'plugin:remove')
+    plugin.handle(this.#context, 'plugin:remove')
     this.plugins = without(this.plugins, plugin)
   }
 
   async handle(event: E, data: any) {
     await Promise.all(
-      this.plugins.map(p => p.handle(this.context, event, data)),
+      this.plugins.map(p => p.handle(this.#context, event, data)),
     )
   }
 }
