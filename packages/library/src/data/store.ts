ee1f6381023d1d88646da03933f85f98845fd529
Felix Henninger <mailbox@felixhenninger.com>
2023-02-14 14:50:55 +0100

    Formatting update in data.store module

--- packages/library/src/data/store.ts
+++ packages/library/src/data/store.ts
@@ -69,7 +69,11 @@ export class Store<R extends Row = Row> extends Emitter {
   // Get and set individual values ------------------------
   set(key: Partial<R>): void
   set(key: keyof R, value: any, suppressSetTrigger?: boolean): void
-  set(key: keyof R | Partial<R>, value?: any, suppressSetTrigger = false): void {
+  set(
+    key: keyof R | Partial<R>,
+    value?: any,
+    suppressSetTrigger = false,
+  ): void {
     let partial: Partial<R> = {}
     if (typeof key === 'object') {
       partial = key
