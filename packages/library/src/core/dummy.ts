072be7fcd2d51909ab83bec1482927c9e8ec4f76
Felix Henninger <mailbox@felixhenninger.com>
2023-04-19 16:32:53 +0200

    Document dummy component

--- packages/library/src/core/dummy.ts
+++ packages/library/src/core/dummy.ts
@@ -1,5 +1,16 @@
 import { Component, ComponentOptions } from './component'
 
+/**
+ * Dummy component class
+ *
+ * This type of component does not do anything, it skips directly on to
+ * the following component.
+ *
+ * The `Dummy` component is used largely for testing purposes, and not
+ * in any actual studies.
+ *
+ * @internal
+ */
 export class Dummy extends Component {
   constructor(options: Partial<ComponentOptions> = {}) {
     super({ ...options, skip: true })
