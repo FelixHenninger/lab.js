28f50f0b3ab346a00653a8084d467d02ef6db98b
Felix Henninger <mailbox@felixhenninger.com>
2023-06-05 17:42:13 +0200

    Bump version to 22.0-beta8

--- packages/builder/src/logic/io/upgrade.js
+++ packages/builder/src/logic/io/upgrade.js
@@ -580,6 +580,10 @@ const updates = {
       ...data,
       version: [22, 'beta', 7],
     }),
+    '22.beta.7': data => ({
+      ...data,
+      version: [22, 'beta', 8],
+    }),
     // TODO: Add shortcut for folks upgrading from v21 stable to v22
   })
 }
--- packages/builder/src/reducers/version.js
+++ packages/builder/src/reducers/version.js
@@ -1,4 +1,4 @@
-const defaultState = [22, 'beta', 7]
+const defaultState = [22, 'beta', 8]
 
 export default (state=defaultState, action) =>
   state
